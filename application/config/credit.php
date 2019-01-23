2d2463412a6a6db527a46c4cede1eb4a9155a150
Ubuntu <ubuntu@ip-192-168-40-2.ap-northeast-1.compute.internal>
2019-01-23 23:46:36 +0800

    修改

--- application/config/credit.php
+++ application/config/credit.php
@@ -1,4 +1,4 @@
-<?php  if ( ! defined('BASEPATH']] exit('No direct script access allowed'];
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
 $config['credit_level_1']= [
