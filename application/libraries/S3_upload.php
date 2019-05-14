48cef220af52e3d93e92cf91aa5abba4606d5034
Brian <brian@influxfin.com>
2019-05-14 16:51:39 +0800

    降低照片檔名重複率

--- application/libraries/S3_upload.php
+++ application/libraries/S3_upload.php
@@ -166,7 +166,7 @@ class S3_upload {
 				ob_end_clean();
 				$result = $this->client->putObject(array(
 					'Bucket' 		=> S3_BUCKET,
-					'Key'    		=> $type.'/'.$name.$user_id.time().round(microtime(true) * 1000).rand(1,99).'.jpg',
+					'Key'    		=> $type.'/'.$name.$user_id.round(microtime(true) * 1000).rand(1,99).'.jpg',
 					'Body'   		=> $image_data
 				));
 
