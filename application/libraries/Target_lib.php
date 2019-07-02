5fd86dbfa73fe78f51c36c7918939eedbb34fcf2
Brian <brian@influxfin.com>
2019-07-03 04:06:45 +0800

    法人地址列選填

--- application/libraries/Target_lib.php
+++ application/libraries/Target_lib.php
@@ -839,9 +839,11 @@ class Target_lib{
 						$finish		 		= true;
 						foreach($certifications as $certification){
 							if(in_array($certification['id'],$product_certification) && $certification['user_status']!='1'){
-                                $finish	= false;
-								if($get_amount < 30000 && $product_list[$product_id]['id'] == 4 && $certification['id'] == 9){
+								if($get_amount < 31083 && $product_list[$product_id]['id'] == 4 && $certification['id'] == 9){
                                     $finish	= true;
+                                }
+								else{
+                                    $finish	= false;
                                 }
 							}
 						}
