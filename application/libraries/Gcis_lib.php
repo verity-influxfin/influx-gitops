f04ec2a2e69fdd058bb5d602544218ecfcbd7bca
hsiang <ethanwu919@gmail.com>
2019-07-09 11:42:14 +0800

    可顯示商業司資料(商業登記)

--- application/libraries/Gcis_lib.php
+++ application/libraries/Gcis_lib.php
@@ -30,42 +30,20 @@ class Gcis_lib
 
     //商業統編查詢
     public function account_info_businesss($account_no=''){
-        $Agency = [
-            '376410000A' => '新北市政府經濟發展局',
-            '379100000G' => '臺北市商業處',
-            '383100000G' => '高雄市政府',
-            '376570000A' => '基隆市政府',
-            '376580000A' => '新竹市政府',
-            '376590000A' => '臺中市政府',
-            '376610000A' => '臺南市政府',
-            '376600000A' => '嘉義市政府',
-            '376430000A' => '桃園市政府',
-            '376440000A' => '新竹縣政府',
-            '376420000A' => '宜蘭縣政府',
-            '376450000A' => '苗栗縣政府',
-            '376470000A' => '彰化縣政府',
-            '376480000A' => '南投縣政府',
-            '376490000A' => '雲林縣政府',
-            '376500000A' => '嘉義縣政府',
-            '376530000A' => '屏東縣政府',
-            '376560000A' => '澎湖縣政府',
-            '376550000A' => '花蓮縣政府',
-            '376540000A' => '臺東縣政府',
-            '371010000A' => '福建省金門縣政府',
-            '371030000A' => '福建省連江縣政府',
-        ];
+        $Agency = $this->CI->config->item('Agency');
         if(strlen($account_no)==8){
-            foreach($Agency as $key => $value){
+            foreach($Agency as $key=>$value){
                 $url 	= $this->url.'7E6AFA72-AD6A-46D3-8681-ED77951D912D?$format=json&$filter=President_No%20eq%20'.$account_no.'%20and%20Agency%20eq%20'.$value.'&$skip=0&$top=50';
                 $rs  	= curl_get($url);
-                $rs 	= json_decode($rs,TRUE);
-                if($rs && isset($rs[0])){
+				$rs 	= json_decode($rs,TRUE);
+				
+                if(isset($rs[0])){
                     $data = $rs[0];
                     //換成西元年
-                    $data['Company_Setup_Date'] 		= $data['Company_Setup_Date']?date('Y-m-d',strtotime(intval($data['Company_Setup_Date'])+19110000)):'';
-                    $data['Change_Of_Approval_Data'] 	= $data['Change_Of_Approval_Data']?date('Y-m-d',strtotime(intval($data['Change_Of_Approval_Data'])+19110000)):'';
-                    return $data;
-                }
+                    $data['Business_Setup_Approve_Date'] 		= $data['Business_Setup_Approve_Date']?date('Y-m-d',strtotime(intval($data['Business_Setup_Approve_Date'])+19110000)):'';
+                    $data['Business_Last_Change_Date'] 	= $data['Business_Last_Change_Date']?date('Y-m-d',strtotime(intval($data['Business_Last_Change_Date'])+19110000)):'';
+                     return   $data;
+                 }
             }
         }
         return false;
