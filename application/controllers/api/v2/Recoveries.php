b7f305773e4835ab7cffe1a4bb3dc56a90ae7615
Brian <brian@influxfin.com>
2019-07-17 16:01:44 +0800

    不開放批次債轉

--- application/controllers/api/v2/Recoveries.php
+++ application/controllers/api/v2/Recoveries.php
@@ -1459,8 +1459,7 @@ class Recoveries extends REST_Controller {
 		}
 		
 		$ids = explode(',',$input['ids']);
-        $count 	= count($ids);
-		if(!empty($ids)){//
+		if(!empty($ids)&&count($ids)==1){//
 			foreach($ids as $key => $id){
 				$id = intval($id);
 				if(empty($id)){
