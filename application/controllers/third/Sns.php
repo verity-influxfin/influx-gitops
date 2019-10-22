f2a92869d3182957d3049d2671adf6648cf9216a
祥 <hsiangwu@influxfin.com>
2019-10-22 22:46:03 +0800

    Sns Https驗證

--- application/controllers/third/Sns.php
+++ application/controllers/third/Sns.php
@@ -8,13 +8,14 @@ class Sns extends REST_Controller {
     public function __construct()
     {
         parent::__construct();
-		
+		$this->load->library('S3_lib');
 
     }
 
 
-	public function certification_post()
-    {
+	public function credit_post()
+	{
+		//第一次SNS認證 ＋＋
 		$rawData = file_get_contents('php://input');
 		$_POST = json_decode($rawData);
 		$ch = curl_init($_POST->SubscribeURL) ;
@@ -22,6 +23,12 @@ class Sns extends REST_Controller {
 		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
 		curl_exec($ch) ;
 		curl_close($ch);
-    }
+		//第一次SNS認證 --
+		//$this->s3_lib->get_reply_mail();
+
+
+
+
+	}
 
 }
