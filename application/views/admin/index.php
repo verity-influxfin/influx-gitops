946c869482bdd4672be56d6aed99c6f9f936f472
allenmobile01 <allen@mobile01.com>
2019-11-06 10:04:41 +0800

    Added evaluation link on dashboard

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -205,6 +205,29 @@
                     </a>
                 </div>
             </div>
+			<div class="col-lg-3 col-md-6"></div>
+			<div class="col-lg-3 col-md-6">
+				<div class="panel panel-yellow">
+					<div class="panel-heading">
+						<div class="row">
+							<div class="col-xs-3">
+								<i class="fa fa-shield fa-5x"></i>
+							</div>
+							<div class="col-xs-9 text-right">
+								<div class="huge"><?=$target_count["evaluation"];?></div>
+								<div>待二審</div>
+							</div>
+						</div>
+					</div>
+					<a href="<?=admin_url('Target/waiting_evaluation') ?>">
+						<div class="panel-footer">
+							<span class="pull-left">View Details</span>
+							<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+							<div class="clearfix"></div>
+						</div>
+					</a>
+				</div>
+			</div>
 		</div>
 		<!-- /.row -->
 		<div class="row">
