e7b8dc257c79bd78e8645608fe74a1e952fd3cd9
allenmobile01 <allen@mobile01.com>
2019-11-07 11:51:58 +0800

    Uncomment codes in joint credit lib

--- application/libraries/Joint_credit_lib.php
+++ application/libraries/Joint_credit_lib.php
@@ -12,21 +12,21 @@ class Joint_credit_lib{
 	}
 
 	public function check_join_credits($text, &$result){
-//		$this->check_bank_loan($text, $result);
-//		$this->check_overdue_and_bad_debts($text, $result);
-//		$this->check_main_debts($text, $result);
-//		$this->check_extra_debts($text, $result);
-//		$this->check_extra_transfer_debts($text,$result);
-//		$this->check_bounced_checks($text, $result);
-//		$this->check_lost_contacts($text, $result);
-//		$this->check_credit_cards($text, $result);
-//		$this->check_credit_card_accounts($text, $result);
-//		$this->check_credit_card_debts($text, $result);
+		$this->check_bank_loan($text, $result);
+		$this->check_overdue_and_bad_debts($text, $result);
+		$this->check_main_debts($text, $result);
+		$this->check_extra_debts($text, $result);
+		$this->check_extra_transfer_debts($text,$result);
+		$this->check_bounced_checks($text, $result);
+		$this->check_lost_contacts($text, $result);
+		$this->check_credit_cards($text, $result);
+		$this->check_credit_card_accounts($text, $result);
+		$this->check_credit_card_debts($text, $result);
 		$this->check_browsed_hits($text, $result);
-//		$this->check_browsed_hits_by_electrical_pay($text, $result);
-//		$this->check_browsed_hits_by_itself($text, $result);
-//		$this->check_extra_messages($text, $result);
-//		$this->check_credit_scores($text, $result);
+		$this->check_browsed_hits_by_electrical_pay($text, $result);
+		$this->check_browsed_hits_by_itself($text, $result);
+		$this->check_extra_messages($text, $result);
+		$this->check_credit_scores($text, $result);
 		return $result;
 	}
 
