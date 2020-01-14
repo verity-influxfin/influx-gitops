50576d3e18221266b251a9463e7e3cdf5cb7de36
Brian <brian@influxfin.com>
2020-01-14 16:00:19 +0800

    未上傳法人影片隱藏按鈕

--- application/views/admin/judicial_person/judicial_person_list.php
+++ application/views/admin/judicial_person/judicial_person_list.php
@@ -99,6 +99,7 @@
 										if(isset($list) && !empty($list)){
 											$count = 0;
 											foreach($list as $key => $value){
+                                                $media_data =str_replace(']},',"",strrchr(urldecode($value->sign_video),']}'));
 												$count++;
 									?>
                                         <tr class="<?=$count%2==0?"odd":"even"; ?> list">
@@ -113,10 +114,10 @@
                                             <td><?=isset($value->cooperation_address)?$value->cooperation_address:"" ?></td>
                                             <td><?=isset($value->remark)?$value->remark:"" ?></td>
                                             <td><?=isset($status_list[$value->status])?$status_list[$value->status]:"" ?>
-											<? if($value->status==0){ ?>
+											<? if($value->status==0 && $media_data != ']}'){ ?>
 												<button class="btn btn-success" onclick="success(<?=isset($value->id)?$value->id:"" ?>)">通過</button>
 												<button class="btn btn-danger" onclick="failed(<?=isset($value->id)?$value->id:"" ?>)">不通過</button>
-											<? } ?>
+											<? } elseif($value->status==0){echo '未上傳法人影片';} ?>
 											</td>
                                             <td><?=isset($value->created_at)?date("Y-m-d H:i:s",$value->created_at):"" ?></td>
 											<td><a target="_blank" href="<?=admin_url('judicialperson/edit')."?id=".$value->id ?>" class="btn btn-default">管理</a></td>
