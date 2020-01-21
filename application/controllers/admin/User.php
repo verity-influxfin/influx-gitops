67ab2216aa9c0b7d22b92b5f34e06a30222f2c19
Brian <brian@influxfin.com>
2020-01-21 18:33:49 +0800

    投資端回傳當時案件認證狀態 #156

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -291,111 +291,12 @@ class User extends MY_Admin_Controller {
 		$this->json_output->setStatusCode(200)->setResponse(["block" => $this->block_output->toOne()])->send();
 	}
 
-	public function related_users($get_result=false)
+	public function related_users()
 	{
 		$input = $this->input->get(NULL, TRUE);
+		$this->load->library('Anti_fraud_lib');
+		$this->anti_fraud_lib->related_users($input["id"]);
 
-		$userId = isset($input["id"])
-			? intval($input["id"])
-			: $get_result?$get_result:0;
-
-		$this->load->library('output/json_output');
-		if ($userId <= 0) {
-			return $this->json_output->setStatusCode(400)->setErrorCode(ArgumentError)->send();
-		}
-
-		$this->load->model('mongolog/user_login_log_model');
-
-		$usersWithSameDeviceId = [];
-		$userIds = $this->user_login_log_model->findUserIdsHavingSameDeviceIdsWith($userId);
-		if ($userIds && $userIds[0]->users) {
-			$usersWithSameDeviceId = $this->user_model->get_many_by(["id" => $userIds[0]->users]);
-		}
-
-		$usersWithSameIp = [];
-		$timeBefore = 1564102800;
-		$userWithIps = $this->user_login_log_model->findUserLoginIps($userId, $timeBefore);
-		if ($userWithIps) {
-			$user = $this->user_login_log_model->findUserIdsByIps($userWithIps->created_ips, $timeBefore);
-			$userIdsWithSameIp = $this->user_model->get_many_by(["id" => $user->users]);
-		}
-
-		$usersWithSameEmergencyContact = $this->user_meta_model->get_users_with_same_emergency_contact($userId);
-
-		$emergencyContact = $this->user_meta_model->get_emergency_contact_who_is_member($userId);
-
-		$this->load->model('user/user_certification_model');
-		$certificationRequests = $this->user_certification_model->get_many_by([
-			'user_id' => $userId,
-			'certification_id' => [1,3,6],
-		]);
-
-		$addresses = [];
-		$bankAccounts = [];
-		$idCardNumbers = [];
-		$emails = [];
-		foreach ($certificationRequests as $certificationRequest) {
-			$content = json_decode($certificationRequest->content);
-			$certificationId = $certificationRequest->certification_id;
-			if ($certificationId == 1) {
-				$idCardNumbers[] = $content->id_number;
-				$addresses[] = $content->address;
-			} elseif ($certificationId == 3) {
-				$bankAccounts[] = $content->bank_account;
-			} elseif ($certificationId == 6) {
-				$emails[] = $content->email;
-			}
-		}
-
-		$usersWithSameBankAccount = $this->user_certification_model->get_users_with_same_value($userId, 'bank_account', $bankAccounts);
-		$usersWithSameIdNumber = $this->user_certification_model->get_users_with_same_value($userId, 'id_number', $idCardNumbers);
-
-		$potentialPhoneNumbers = [];
-		foreach ($emails as $email) {
-			preg_match('/[0|886][0-9]{8,9}/', $email, $matches);
-			foreach ($matches as $phone) {
-				$potentialPhoneNumbers[] = $phone;
-			}
-		}
-		$usersWithSamePhoneNumber = [];
-		if ($potentialPhoneNumbers) {
-			$usersWithSamePhoneNumber = $this->user_model->get_many_by([
-				'phone' => $potentialPhoneNumbers,
-				'id !=' => $userId
-			]);
-		}
-
-		$usersWithSameAddress = $this->user_certification_model->get_users_with_same_value($userId, 'address', $addresses);
-
-		$currentUser = $this->user_model->get($userId);
-		$introducer = [];
-		if ($currentUser->promote_code) {
-			$introducer =$this->user_model->get_by(['promote_code' => $currentUser->promote_code]);
-		}
-
-		$data = new stdClass();
-		$data->same_device_id = $usersWithSameDeviceId;
-		$data->same_ip = $usersWithSameIp;
-		$data->samp_contact = $usersWithSameEmergencyContact;
-		$data->emergency_contact = $emergencyContact;
-		$data->same_bank_account = $usersWithSameBankAccount;
-		$data->same_id_number = $usersWithSameIdNumber;
-		$data->same_phone_number = $usersWithSamePhoneNumber;
-		$data->same_address = $usersWithSameAddress;
-		$data->introducer = $introducer;
-
-		$this->load->library('output/user/related_user_output', ["data" => $data]);
-
-		$relatedUsers = $this->related_user_output->toMany();
-
-		if($get_result){
-			return $relatedUsers;
-		}
-
-		if (!$relatedUsers) {
-			$this->json_output->setStatusCode(204)->send();
-		}
-		$this->json_output->setStatusCode(200)->setResponse(["related_users" => $relatedUsers])->send();
 	}
 
 	public function migrateData()
