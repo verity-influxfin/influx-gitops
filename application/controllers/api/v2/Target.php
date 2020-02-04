f7eed2cde4e7e3e6f6067cc2441dfdf850ee7d90
Brian <brian@influxfin.com>
2020-02-04 22:37:40 +0800

    修正未做學生認證於上架後造成顯示錯誤#191

--- application/controllers/api/v2/Target.php
+++ application/controllers/api/v2/Target.php
@@ -138,7 +138,13 @@ class Target extends REST_Controller {
 					$age  = get_age($user_info->birthday);
 					if($product_list[$value->product_id]['identity']==1){
 						$user_meta 	            = $this->user_meta_model->get_by(['user_id'=>$value->user_id,'meta_key'=>'school_name']);
-                        $user_meta->meta_value 	= preg_replace('/\(自填\)/', '',$user_meta->meta_value);
+                        if(is_object($user_meta)){
+                            $user_meta->meta_value =preg_replace('/\(自填\)/', '',$user_meta->meta_value);
+                        }
+                        else{
+                            $user_meta = new stdClass();
+                            $user_meta->meta_value='未提供學校資訊';
+                        }
 					}else{
 						$user_meta 	= $this->user_meta_model->get_by(['user_id'=>$value->user_id,'meta_key'=>'company_name']);
 					}
