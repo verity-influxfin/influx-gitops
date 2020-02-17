700e7865c737e86c37039a0d2872d8f81b10d63a
alleninflux <allen@influxfin.com>
2020-02-17 21:49:41 +0800

    Modified student card identification #150

---
+++ application/controllers/api/v2/Image.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require(APPPATH.'/libraries/REST_Controller.php');
+
+class Image extends REST_Controller {
+
+	public $user_info;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('S3_upload');
+        $this->load->library('S3_lib');
+        $this->load->library('log/log_image_model');
+    }
+
+    public function info_delete()
+    {
+        $input = $this->input->get(NULL, TRUE);
+        $url = isset($input["url"]) ? $input["url"] : 0;
+        $ownerId = isset($input["owner_id"]) ? intval($input["owner_id"]) : 0;
+
+        if ($url || $ownerId <= 0) {
+            $this->response(['result' => 'ERROR','error' => INPUT_NOT_CORRECT]);
+        }
+
+        $url = str_replace(FRONT_CDN_URL, INFLUX_S3_URL, $url);
+
+        $log = $this->log_image_model->get_by([
+            "user_id" => $ownerId,
+            "url" => $url
+        ]);
+        if (!$log) {
+            $this->response(['result' => 'ERROR','error' => INPUT_NOT_CORRECT]);
+        }
+
+        $allowedToDeleteTypes = ["temp_image"];
+        $isAllowedToDelete = in_array($log->type, $allowedToDeleteTypes);
+        if (!$isAllowedToDelete) {
+            $this->response(['result' => 'ERROR','error' => INPUT_NOT_CORRECT]);
+        }
+
+        $this->CI->s3_lib->public_delete_image($log["url"], FRONT_S3_BUCKET);
+        $this->response(['result' => 'SUCCESS']);
+    }
+}
--- application/libraries/Image_recognition_lib.php
+++ application/libraries/Image_recognition_lib.php
@@ -11,21 +11,47 @@ class Image_recognition_lib
         $this->ml_url = "http://" . getenv('GRACULA_IP') . ":" . getenv('GRACULA_PORT') . "/ml/api/v1.0/";
     }
 
-    public function readStudentCard($image)
+    public function requestStudentCardIdentification($image, $ownerId)
+    {
+        if(empty($image) || $ownerId <= 0) {
+            return false;
+        }
+
+        $newImageUrl = $this->CI->s3_upload->public_image_by_data(
+            file_get_contents($image->url),
+            FRONT_S3_BUCKET,
+            $ownerId,
+            [
+                'type' => 'stmps/students',
+                'name' => md5($image->url) . '.jpg',
+            ]
+        );
+        $newImageUrl = str_replace(INFLUX_S3_URL, FRONT_CDN_URL, $newImageUrl);
+
+        $url = $this->ml_url  . "student-cards/{$image->id}";
+
+        $data = ["card_url" => $newImageUrl, "owner_id" => intval($ownerId)];
+
+        $result = curl_get($url, $data);
+        $response = json_decode($result);
+
+        if (!$result || !isset($response->status) || $response->status != 200) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getStudentCardIdentification($image)
     {
         if(empty($image)) {
             return;
         }
 
-        $newImageUrl = $this->CI->s3_upload->public_image_by_data(file_get_contents($image), FRONT_S3_BUCKET);
+        $url = $this->ml_url  . "student-cards/{$image->id}";
 
-        $url = $this->ml_url  . "student-cards";
-        $data = ["card_url" => $newImageUrl];
-
-        $result = curl_get($url, $data);
+        $result = curl_get($url);
         $response = json_decode($result);
-
-        $this->CI->s3_lib->public_delete_image($newImageUrl, FRONT_S3_BUCKET);
 
         if (!$result || !isset($response->status) || $response->status != 200) {
             return;
