56c93a6089be259c3b8d4b35012c7271473c6369
alleninflux <allen@influxfin.com>
2020-02-21 19:58:42 +0800

    Modified image removal logic #150

--- application/controllers/api/v2/Image.php
+++ application/controllers/api/v2/Image.php
@@ -20,7 +20,7 @@ class Image extends REST_Controller {
         $url = isset($input["url"]) ? $input["url"] : 0;
         $ownerId = isset($input["owner_id"]) ? intval($input["owner_id"]) : 0;
 
-        if ($url || $ownerId <= 0) {
+        if (!$url || $ownerId <= 0) {
             $this->response(['result' => 'ERROR','error' => INPUT_NOT_CORRECT]);
         }
 
@@ -40,7 +40,7 @@ class Image extends REST_Controller {
             $this->response(['result' => 'ERROR','error' => INPUT_NOT_CORRECT]);
         }
 
-        $this->CI->s3_lib->public_delete_image($log["url"], FRONT_S3_BUCKET);
+        $this->s3_lib->public_delete_image($log->url, FRONT_S3_BUCKET, ["name" => 'student_card_image']);
         $this->response(['result' => 'SUCCESS']);
     }
 }
