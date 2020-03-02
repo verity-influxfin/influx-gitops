c4524a6314f532ecc848d99bea1776117daae498
Brian <brian@influxfin.com>
2020-03-02 11:16:04 +0800

    作廢有效認證新規則之聯徵、工作認證邏輯 #322

--- application/libraries/Certification_lib.php
+++ application/libraries/Certification_lib.php
@@ -1275,14 +1275,19 @@ class Certification_lib{
                 'investor' => $investor,
                 'status !=' => 2,
             ]);
-            if($certification) {
+            if ($certification) {
                 foreach ($certification as $key => $value) {
-                    if ($value->expire_time <= time() && $investor == 0 && !in_array($value->certification_id, [IDCARD, DEBITCARD, EMERGENCY, EMAIL])) {
+                    if ($investor == 0 && !in_array($value->certification_id, [IDCARD, DEBITCARD, EMERGENCY, EMAIL])
+                        && $value->expire_time <= time()
+                    || in_array($value->certification_id, [INVESTIGATION, JOB])
+                        && $value->status == 1 && time() > strtotime('+2 months', $value->updated_at)) {
                         $this->set_failed($value->id, '認證已逾期。', true);
                     }
                 }
             }
         }
+//        1577895400
+//1580313600
         return false;
     }
 
