4764306a184e15a8ed62e04f40d0051513d843b6
alleninflux <allen@influxfin.com>
2020-04-09 11:16:06 +0800

    Modified assets_export column names and values #454

--- application/libraries/utility/Payment_time_utility.php
+++ application/libraries/utility/Payment_time_utility.php
@@ -2,6 +2,31 @@
 
 class Payment_time_utility
 {
+    public function goToPrevious($date)
+    {
+        $day = substr($date, -2);
+        $allNumber = explode("-", $date);
+        if (!$allNumber || count($allNumber) < 3) {
+            return $date;
+        }
+        if ($day > 10) {
+            $allNumber[2] = '10';
+        } elseif ($day < 10) {
+            $allNumber[2] = '10';
+            if ($allNumber[1] == '01') {
+                $allNumber[1] = '12';
+                $allNumber[0]--;
+            } else {
+                $allNumber[1]--;
+            }
+            if (strlen($allNumber[1]) == 1) {
+                $allNumber[1] = '0' . $allNumber[1];
+            }
+        }
+        $date = $allNumber[0] . '-' . $allNumber[1] . '-' . $allNumber[2];
+        return $date;
+    }
+
     public function goToNext($date, $forceToNext = false)
     {
         $day = substr($date, -2);
