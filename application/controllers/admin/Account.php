1ddb338d8710748c9b5ef429530e3ac88efaa133
Brian <brian@influxfin.com>
2020-07-24 19:24:43 +0800

    law trigger #643

--- application/controllers/admin/Account.php
+++ application/controllers/admin/Account.php
@@ -428,8 +428,8 @@ class Account extends MY_Admin_Controller {
 					if($user_to_info){
 						foreach($user_to_info as $k =>$v){
 							$sub_list[] = array(
-								"user_to"				=> $user_name[$v['user_to']],
-								"v_bank_account_to"		=> $v['v_bank_account_to'],
+								"user_to"				=> isset($v['user_to'])?$v['user_to']:'',
+								"v_bank_account_to"		=> isset($v['v_bank_account_to'])?$v['v_bank_account_to']:'',
 								"v_amount_to"			=> $v['principal'] + $v['interest'] + $v['delay_interest'] - $v['platform_fee'],
 								"principal"				=> $v['principal'],
 								"interest"				=> $v['interest'],
