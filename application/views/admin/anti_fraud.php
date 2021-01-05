c39ac0eac6a0dd779f2547c9202c79b749e4e46b
nabroux-influx <nabroux@influxfin.com>
2021-01-05 22:05:52 +0800

    Add - anti-fraud ENV related files

--- application/views/admin/anti_fraud.php
+++ application/views/admin/anti_fraud.php
@@ -4,5 +4,6 @@
     <div id="anti-fraud-app"></div>
 </div>
 
+<script src="<?= base_url() ?>assets/admin/js/anti_fraud_env.js"></script>
 <script src="<?= base_url() ?>assets/admin/js/main.js"></script>
 <script src="<?= base_url() ?>assets/admin/js/vendors.bundle.js"></script>
