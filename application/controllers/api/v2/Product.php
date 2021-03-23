67661ed2d0df7821a541cfba8bf93697d8b7a672
kevin-influx <kevin@influxfin.com>
2021-03-23 16:13:29 +0800

    Add api return type data

--- application/controllers/api/v2/Product.php
+++ application/controllers/api/v2/Product.php
@@ -1093,12 +1093,14 @@ class Product extends REST_Controller {
 					// 返回認證資料
 					$user_certification = $this->user_certification_model->get_by(['id'=>$value['certification_id']]);
 					$content_array_data = [];
+					$content_key = ['labor_type','return_type','mail_file_status'];
 					// 刪除無用資訊
 					if(isset($user_certification->content) && $user_certification->content != '' ){
 						$user_certification = json_decode($user_certification->content,true);
-						if(array_key_exists('mail_file_status',$user_certification)){
-							$content_array_data['mail_file_status'] = $user_certification['mail_file_status'];
-						}
+						foreach($content_key as $key_name){
+							if(array_key_exists($key_name,$user_certification)){
+								$content_array_data[$key_name] = $user_certification[$key_name];
+							}
 					}
 					if(!$content_array_data){
 						$content_array_data = new StdClass();
