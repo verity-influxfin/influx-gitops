88052b1d1094d5a7b982cf2f84f1f4b39e6c1e23
Alesis M1 Active <alesis.m1active2@gmail.com>
2021-05-29 20:47:39 +0800

    New button component

---
+++ resources/js/frontdesk/component/alesis/index/button.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="button">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "Button",
+    props: {
+        text: "",
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+    position: relative;
+    padding: 1rem 1.5rem;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    line-height: 1;
+    font-size: 1rem;
+    background-image: url(/images/alesis-button-lines.svg);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    font-size: 1.1rem;
+    font-weight: bold;
+    color: #036eb7;
+}
+</style>
