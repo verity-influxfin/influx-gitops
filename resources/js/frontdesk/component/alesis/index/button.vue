5ce2de836bb5f2e83b63a08f5aeed3ed13756b45
Alesis M1 Active <alesis.m1active2@gmail.com>
2021-06-10 14:51:48 +0800

    Button with props and correct background scaling

--- resources/js/frontdesk/component/alesis/index/button.vue
+++ resources/js/frontdesk/component/alesis/index/button.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="button">
+    <div class="button" :class="{'-fluid': fluid}">
+        <img class="background" src="/images/alesis-button-lines.svg" alt="">
         <slot></slot>
     </div>
 </template>
@@ -8,26 +9,52 @@
 export default {
     name : "Button",
     props: {
-        text: "",
+        text    : "",
+        size    : {
+            default: "1.1rem",
+        },
+        fluid   : {
+            default: false,
+        },
+        yPadding: {
+            default: "0",
+        },
+        xPadding: {
+            default: "0",
+        },
+    },
+    mounted() {
+        this.$el.style.setProperty('--font-size', this.size)
+        this.$el.style.setProperty('--x-padding', this.xPadding)
+        this.$el.style.setProperty('--y-padding', this.yPadding)
     }
 };
 </script>
 
 <style lang="scss" scoped>
 .button {
-    position: relative;
-    padding: 1rem 1.5rem;
-    display: inline-flex;
-    align-items: center;
+    position       : relative;
+    padding        : var(--y-padding) var(--x-padding);
+    display        : inline-flex;
+    align-items    : center;
     justify-content: center;
-    line-height: 1;
-    font-size: 1rem;
-    background-image: url(/images/alesis-button-lines.svg);
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center;
-    font-size: 1.1rem;
-    font-weight: bold;
-    color: #036eb7;
+    line-height    : 1;
+    font-size      : var(--font-size);
+    font-weight    : bold;
+    color          : #036eb7;
+
+    &.-fluid {
+        width: 100%;
+    }
+
+    .background {
+        position: absolute;
+        top     : 0;
+        right   : 0;
+        bottom  : 0;
+        left    : 0;
+        height  : 100%;
+        width   : 100%;
+    }
 }
 </style>
