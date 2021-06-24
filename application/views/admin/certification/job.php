169904fcd7db46059fadbbba1d15ad1df2857fc4
wayne-influx <wayne@influxfin.com>
2021-06-24 19:33:54 +0800

    Fix - 修正個金自動化勞保的過件邏輯(公司相關資訊)

--- application/views/admin/certification/job.php
+++ application/views/admin/certification/job.php
@@ -162,10 +162,10 @@
 													<tr>
 														<td><?=isset($content['pdf_info']['last_insurance_info']['insuranceId']) ? $content['pdf_info']['last_insurance_info']['insuranceId'] : '' ?></td>
 														<td><?=isset($content['pdf_info']['last_insurance_info']['companyName']) ? $content['pdf_info']['last_insurance_info']['companyName'] : '' ?></td>
-														<td><?=isset($content['pdf_info']['last_insurance_info']['detailList'][0]['insuranceSalary']) ? $content['pdf_info']['last_insurance_info']['detailList'][0]['insuranceSalary'] : '' ?></td>
-														<td><?=isset($content['pdf_info']['last_insurance_info']['detailList'][0]['startDate']) ? $content['pdf_info']['last_insurance_info']['detailList'][0]['startDate'] : '' ?></td>
-														<td><?=isset($content['pdf_info']['last_insurance_info']['detailList'][0]['endDate']) ? $content['pdf_info']['last_insurance_info']['detailList'][0]['endDate'] : '' ?></td>
-														<td><?=isset($content['pdf_info']['last_insurance_info']['detailList'][0]['comment']) ? $content['pdf_info']['last_insurance_info']['detailList'][0]['comment'] : '' ?></td>
+														<td><?=isset($content['pdf_info']['last_insurance_info']['detailList']['insuranceSalary']) ? $content['pdf_info']['last_insurance_info']['detailList'][0]['insuranceSalary'] : '' ?></td>
+														<td><?=isset($content['pdf_info']['last_insurance_info']['detailList']['startDate']) ? $content['pdf_info']['last_insurance_info']['detailList'][0]['startDate'] : '' ?></td>
+														<td><?=isset($content['pdf_info']['last_insurance_info']['detailList']['endDate']) ? $content['pdf_info']['last_insurance_info']['detailList'][0]['endDate'] : '' ?></td>
+														<td><?=isset($content['pdf_info']['last_insurance_info']['detailList']['comment']) ? $content['pdf_info']['last_insurance_info']['detailList'][0]['comment'] : '' ?></td>
 													</tr>
 												</tbody>
 											</table>
