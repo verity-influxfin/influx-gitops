5ded0887031efab7b788287b23cc16994f7cf002
Alesis M1 Active <alesis.m1active2@gmail.com>
2021-07-28 10:15:45 +0800

    Updated fake svg path

--- resources/js/frontdesk/component/alesis/horizontal-path.vue
+++ resources/js/frontdesk/component/alesis/horizontal-path.vue
@@ -232,21 +232,21 @@ export default {
                     "title": "BBB",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "CCC",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "DDDD",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 31,
@@ -260,21 +260,21 @@ export default {
                     "title": "FFF",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "GGG",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "HHH",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 31,
@@ -288,21 +288,21 @@ export default {
                     "title": "JJJ",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "KKK",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "LLL",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 31,
@@ -316,35 +316,35 @@ export default {
                     "title": "NNN",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "OOO",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "PPP",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "QQQ",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 },
                 {
                     "ID": 18,
                     "title": "RRR",
                     "hook_date": "2020-12-01",
                     "content": "預計推出上市銀行聯手推廣信貸產品",
-                    "icon": "upload/milestone/patent.svg"
+                    "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
                 }
             ],
             sets: [[]],
