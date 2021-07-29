b9f42240203d210c64876e305228633b4386dc04
Alesis M1 Active <alesis.m1active2@gmail.com>
2021-07-29 15:41:33 +0800

    Index plan data

---
+++ resources/js/frontdesk/data/index_plans.js
@@ -0,0 +1,55 @@
+export default [
+    {
+        header: "學生貸",
+        unready: false,
+        image: "/images/alesis-student-rotate.svg",
+        targets: [
+            "大學、碩博士在學生",
+            "20-35歲中華民國國民"
+        ],
+        action: "開始試算",
+        features: [
+            "Instagram / Dcard 大學生最推薦資金媒合平台",
+            "備妥雙證件即可，不打擾親友",
+            "額度高達12萬",
+            "3-24期分期方式選擇多元",
+            "隱私安全、全線上申請、無人照會",
+        ]
+    },
+    {
+        header: "上班族貸",
+        unready: false,
+        image: "/images/alesis-work-rotate.svg",
+        targets: [
+            "社會新鮮人、金融小白",
+            "20-35歲中華民國國民"
+        ],
+        action: "開始試算",
+        features: [
+            "超過200家上市櫃公司、千大企業員工最佳選擇",
+            "申請紀錄不上聯徵，不影響未來銀行貸款額度",
+            "可貸額度1萬-20萬",
+            "3-24期分期方式選擇多元",
+            "隱私安全、全線上申請、無人照會",
+        ]
+    },
+    {
+        header: "中小企業融資",
+        unready: false,
+        image: "/images/alesis-companies-rotate.svg",
+        targets: [],
+        action: "敬請期待",
+        features: [
+            "信保微企貸",
+            "申請速度最快，核准機率最高",
+        ]
+    },
+    {
+        header: "車輛融資",
+        unready: true,
+        image: "/images/alesis-car-rotate.svg",
+        targets: [],
+        action: "敬請期待",
+        features: []
+    }
+]
