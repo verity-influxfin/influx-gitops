5dd84ed4ad6447fc52b6fde047a19684a64e4333
Alesis M1 Active <alesis.m1active2@gmail.com>
2021-07-29 15:41:21 +0800

    New Roadmap data

---
+++ resources/js/frontdesk/data/index_vertical_roadmap.js
@@ -0,0 +1,128 @@
+export default [
+    {
+        "ID": 31,
+        "title": "AAA",
+        "hook_date": "2021-01-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": null
+    },
+    {
+        "ID": 18,
+        "title": "BBB",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "CCC",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "DDDD",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 31,
+        "title": "EEE",
+        "hook_date": "2021-01-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": null
+    },
+    {
+        "ID": 18,
+        "title": "FFF",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "GGG",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "HHH",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 31,
+        "title": "III",
+        "hook_date": "2021-01-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": null
+    },
+    {
+        "ID": 18,
+        "title": "JJJ",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "KKK",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "LLL",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 31,
+        "title": "MMM",
+        "hook_date": "2021-01-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": null
+    },
+    {
+        "ID": 18,
+        "title": "NNN",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "OOO",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "PPP",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "QQQ",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    },
+    {
+        "ID": 18,
+        "title": "RRR",
+        "hook_date": "2020-12-01",
+        "content": "預計推出上市銀行聯手推廣信貸產品",
+        "icon": "https://www.influxfin.com/upload/milestone/milestone_06_20210305.svg"
+    }
+]
