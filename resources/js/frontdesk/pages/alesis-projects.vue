0ded78eb237c85d27e9a56cc80c52d0608f6487c
Alesis M1 Active <alesis.m1active2@gmail.com>
2021-07-29 23:52:13 +0800

    Using Alesis Project Viewer

--- resources/js/frontdesk/pages/alesis-projects.vue
+++ resources/js/frontdesk/pages/alesis-projects.vue
@@ -22,65 +22,7 @@
             </alesis-header>
             <alesis-section>
                 <alesis-space size="medium"></alesis-space>
-                <div class="包裹容器">
-                    <div class="分類">
-                        <button class="項目 項目_啟用的">
-                            <div class="文字">全部</div>
-                            <div class="標籤">99+</div>
-                        </button>
-                        <button class="項目">
-                            <div class="文字">學生</div>
-                            <div class="標籤">86</div>
-                        </button>
-                        <button class="項目">
-                            <div class="文字">上班族</div>
-                            <div class="標籤">60</div>
-                        </button>
-                    </div>
-                    <div class="排序條件">
-                        <a href="#!" class="項目 項目_啟用的">上架日</a>
-                        <a href="#!" class="項目">金額</a>
-                        <a href="#!" class="項目">期數</a>
-                        <a href="#!" class="項目">年利率</a>
-                        <a href="#!" class="項目">信評</a>
-                        <div class="項目">
-                            <div class="箭頭群組">
-                                <img src="/images/alesis-work-loan-arrow-up.svg" class="上">
-                                <img src="/images/alesis-work-loan-arrow-down.svg" class="下">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="卡片列表">
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                    </div>
-                    <div class="分頁導覽">
-                        <a href="#!" class="項目">上一頁</a>
-                        <a href="#!" class="項目">1</a>
-                        <a href="#!" class="項目">2</a>
-                        <a href="#!" class="項目">3</a>
-                        <a href="#!" class="項目">4</a>
-                        <a href="#!" class="項目">...</a>
-                        <a href="#!" class="項目">30</a>
-                        <a href="#!" class="項目">下一頁</a>
-                    </div>
-                </div>
+                <alesis-project-viewer :status=3></alesis-project-viewer>
                 <alesis-space size="medium"></alesis-space>
             </alesis-section>
         </div>
@@ -93,65 +35,7 @@
             </alesis-header>
             <alesis-section>
                 <alesis-space size="medium"></alesis-space>
-                <div class="包裹容器">
-                    <div class="分類">
-                        <button class="項目 項目_啟用的">
-                            <div class="文字">全部</div>
-                            <div class="標籤">99+</div>
-                        </button>
-                        <button class="項目">
-                            <div class="文字">學生</div>
-                            <div class="標籤">86</div>
-                        </button>
-                        <button class="項目">
-                            <div class="文字">上班族</div>
-                            <div class="標籤">60</div>
-                        </button>
-                    </div>
-                    <div class="排序條件">
-                        <a href="#!" class="項目 項目_啟用的">上架日</a>
-                        <a href="#!" class="項目">金額</a>
-                        <a href="#!" class="項目">期數</a>
-                        <a href="#!" class="項目">年利率</a>
-                        <a href="#!" class="項目">信評</a>
-                        <div class="項目">
-                            <div class="箭頭群組">
-                                <img src="/images/alesis-work-loan-arrow-up.svg" class="上">
-                                <img src="/images/alesis-work-loan-arrow-down.svg" class="下">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="卡片列表">
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                        <div class="項目">
-                            <alesis-project></alesis-project>
-                        </div>
-                    </div>
-                    <div class="分頁導覽">
-                        <a href="#!" class="項目">上一頁</a>
-                        <a href="#!" class="項目">1</a>
-                        <a href="#!" class="項目">2</a>
-                        <a href="#!" class="項目">3</a>
-                        <a href="#!" class="項目">4</a>
-                        <a href="#!" class="項目">...</a>
-                        <a href="#!" class="項目">30</a>
-                        <a href="#!" class="項目">下一頁</a>
-                    </div>
-                </div>
+                <alesis-project-viewer :status=10></alesis-project-viewer>
                 <alesis-space size="medium"></alesis-space>
             </alesis-section>
         </div>
@@ -179,6 +63,7 @@ import AlesisSymcard           from "../component/alesis/AlesisSymcard";
 import AlesisTaiwanMap         from "../component/alesis/AlesisTaiwanMap";
 import AlesisVerticalRoadmap   from "../component/alesis/AlesisVerticalRoadmap";
 import AlesisSpace             from "../component/alesis/AlesisSpace";
+import AlesisProjectViewer     from "../component/alesis/AlesisProjectViewer";
 
 export default {
     components: {
@@ -200,6 +85,10 @@ export default {
         AlesisTaiwanMap,
         AlesisVerticalRoadmap,
         AlesisSpace,
+        AlesisProjectViewer,
+    },
+    props: {
+
     },
     data: () => ({
 
@@ -316,151 +205,5 @@ export default {
 
 .案件區塊 + .案件區塊 {
     margin-top: 3rem;
-}
-
-.案件區塊 .包裹容器 {
-    margin: 0 auto;
-}
-
-.案件區塊 .包裹容器 .分類 {
-    display        : flex;
-    align-items    : center;
-    justify-content: center;
-    gap            : 1rem;
-}
-
-.案件區塊 .包裹容器 .分類 .項目 {
-    border       : 1px solid #326398;
-    border-radius: .4rem;
-    background   : transparent;
-    appearance   : none;
-    padding      : .25rem .8rem;
-    font-size    : 1.5rem;
-    display      : flex;
-    align-items  : center;
-    gap          : .8rem;
-    white-space  : nowrap;
-    color        : #326398;
-
-    @include rwd {
-        font-size: 1rem;
-        padding  : .25rem .5rem;
-        gap      : .4rem;
-    }
-}
-
-.案件區塊 .包裹容器 .分類 .項目.項目_啟用的 {
-    background: #326398;
-    color     : #FFF;
-}
-
-.案件區塊 .包裹容器 .分類 .項目 .標籤 {
-    font-size    : 1rem;
-    border       : 1px solid #326398;
-    padding      : .2rem .5rem;
-    border-radius: .4rem;
-    line-height  : 1;
-    color        : #326398;
-    background   : #FFF;
-
-    @include rwd {
-        font-size: .8rem;
-    }
-}
-
-.案件區塊 .包裹容器 .排序條件 {
-    display        : flex;
-    align-items    : center;
-    justify-content: center;
-    margin-top     : 1rem;
-
-    @include rwd {
-        margin-top: 0;
-    }
-}
-
-.案件區塊 .包裹容器 .排序條件 .項目 {
-    padding        : 1rem 3rem;
-    font-size      : 1.2rem;
-    color          : #AAAAAA;
-    text-decoration: none;
-    white-space    : nowrap;
-
-    @include rwd {
-        padding  : 1rem .7rem;
-        font-size: 1rem;
-    }
-}
-
-.案件區塊 .包裹容器 .排序條件 .項目.項目_啟用的 {
-    color: #4D4D4D;
-}
-
-.案件區塊 .包裹容器 .排序條件 .項目 .箭頭群組 {
-    display       : flex;
-    flex-direction: column;
-}
-
-.案件區塊 .包裹容器 .排序條件 .項目 .箭頭群組 img {
-    width: 16px;
-
-    @include rwd {
-        width: 12px;
-    }
-}
-
-.案件區塊 .包裹容器 .卡片列表 {
-    max-width            : 950px;
-    margin               : 0 auto;
-    display              : grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap                  : 3rem;
-    margin-top           : 2rem;
-
-    @include rwd {
-        grid-template-columns: 1fr;
-    }
-}
-
-.案件區塊 .包裹容器 .卡片列表 > .項目 {
-    @include rwd {
-        display        : flex;
-        align-items    : center;
-        justify-content: center;
-        transform      : scale(0.8) translateX(4.3rem);
-        margin-left    : -7rem;
-        margin-top     : -3rem;
-    }
-}
-
-.案件區塊 .包裹容器 .分頁導覽 {
-    display        : flex;
-    width          : min-content;
-    white-space    : nowrap;
-    margin         : 0 auto;
-    border         : 1px solid #326398;
-    border-radius  : .4rem;
-    align-items    : center;
-    justify-content: center;
-    margin-top     : 2rem;
-}
-
-.案件區塊 .包裹容器 .分頁導覽 .項目 {
-    line-height    : 1;
-    color          : #326398;
-    padding        : .5rem .7rem;
-    font-size      : 1.1rem;
-    border-right   : 1px solid #326398;
-    vertical-align : top;
-    text-decoration: none;
-
-    @include rwd {
-        font-size: 1rem;
-        padding  : .5rem .5rem;
-    }
-}
-
-.案件區塊 .包裹容器 .分頁導覽 .項目:last-child {
-    border-right: 0;
 }
 </style>
