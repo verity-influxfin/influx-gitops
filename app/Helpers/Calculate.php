4f4b4f68ad15476e1b481fea7a7eefcc9d0925dd
kevin-influx <kevin@influxfin.com>
2021-08-06 18:58:00 +0800

    Fix pmt calculate function
    修正 - 本息平均攤還公式期數錯誤問題

--- app/Helpers/Calculate.php
+++ app/Helpers/Calculate.php
@@ -7,7 +7,7 @@ class Calculate
 	/**
      * [PMT_calculate 本息均攤公式]
      * @param integer $rate [年利率]
-     * @param integer $nper [總期數]
+     * @param integer $nper [總期數(一個月為一期)]
      * @param integer $pv   [金額]
      */
 	public function PMT_calculate(int $rate = 0,int $nper = 0,int $pv = 0){
@@ -15,8 +15,7 @@ class Calculate
 			return '期數與貸款金額不得為0';
 		}
 		$month_rate = $rate / 100 / 12;
-		$months = $nper * 12;
-		$response = pow((1 + $month_rate),$months) * $month_rate / (pow((1 + $month_rate),$months) -1) * $pv;
+		$response = pow((1 + $month_rate),$nper) * $month_rate / (pow((1 + $month_rate),$nper) -1) * $pv;
 		return $response;
 	}
 }
