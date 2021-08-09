33b9455cc9c9fbe7740aa3a1503674a21949b155
wayne-influx <wayne@influxfin.com>
2021-08-09 12:39:40 +0800

    Fix - 增加取得並設定虛擬帳號為使用中的方法

--- application/models/user/Virtual_account_model.php
+++ application/models/user/Virtual_account_model.php
@@ -29,4 +29,47 @@ class Virtual_account_model extends MY_Model
         return $data;
     }
 
+    /**
+     * 設定虛擬帳號的使用狀態 (成功:回傳虛擬帳號的物件/不成功:回傳空陣列)
+     * @param $user_id
+     * @param $investor
+     * @param $old_status
+     * @param $new_status
+     * @param $virtual_prefix: 虛擬帳號前綴數字
+     * @return array|stdClass
+     */
+    public function setVirtualAccount($user_id, $investor, $old_status, $new_status, $virtual_prefix) {
+        $conditions = [
+            'user_id' => $user_id,
+            'investor' => $investor,
+            'status' => $old_status,
+            'virtual_account like ' => $virtual_prefix.'%'
+        ];
+
+        $this->db->trans_start();
+        $this->db->select_for_update('*')->where($conditions);
+        $virtual_account = $this->db->get($this->_table)->result();
+
+        if(is_array($virtual_account) && !empty($virtual_account)) {
+            $virtual_account = $virtual_account[0];
+            $result = $this->db->where(['id' => $virtual_account->id])
+                ->set(['status' => $new_status])
+                ->update($this->_table);
+
+            // 更新失敗需回傳 empty array
+            if($this->db->affected_rows() > 0) {
+                $virtual_account->status = $new_status;
+            }else{
+                $virtual_account = [];
+            }
+        }
+        $this->db->trans_complete();
+
+        if($this->db->trans_status() === FALSE) {
+
+            $virtual_account = [];
+        }
+
+        return $virtual_account;
+    }
 }
