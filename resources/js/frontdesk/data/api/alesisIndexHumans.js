7f1ba1284e24d032e80ea5de53902b4bdeb00298
Alesis M1 Active <alesis.m1active2@gmail.com>
2021-08-12 12:00:10 +0800

    fake data

---
+++ resources/js/frontdesk/data/api/alesisIndexHumans.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        "ID": 2,
+        "feedback": "在普匯投資除了可以有穩定的收益外，還可以幫助到有夢的大學生，所以覺得這個平台還滿特別的",
+        "imageSrc": null,
+        "video_link": "https://www.youtube.com/embed/4RqbVmH6aHU",
+        "post_title": "台北科技業 劉先生",
+        "rank": "officeWorker",
+        "type": "【投資人專訪】"
+    },
+    {
+        "ID": 3,
+        "feedback": "有比較過其他平台，其他的債權金額都比較大，普匯可以小額投資，還可以進行債權轉讓，讓我可以即時的拿回投資的錢",
+        "imageSrc": null,
+        "video_link": "https://www.youtube.com/embed/we16JV8Hc1o",
+        "post_title": "台北設計業 陳先生",
+        "rank": "officeWorker",
+        "type": "【投資人專訪】"
+    },
+    {
+        "ID": 4,
+        "feedback": "在普匯上投資，平均都可以獲得12%的年化報酬率 和以往投資的經驗比較起來， 報酬率跟風險管控都更加穩健！ 否則平常上班也無法一直盯盤注意市場變化",
+        "imageSrc": null,
+        "video_link": "https://www.youtube.com/embed/tsd-YTxzRyI",
+        "post_title": "台北服飾業 何小姐",
+        "rank": "officeWorker",
+        "type": "【投資人專訪】"
+    }
+]
