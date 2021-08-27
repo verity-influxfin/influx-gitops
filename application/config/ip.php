41f10c7a0d8acb5709d27e8f1e92e98582f7465b
kevin-influx <kevin@influxfin.com>
2021-08-27 10:05:03 +0800

    Stash save

--- application/config/ip.php
+++ application/config/ip.php
@@ -2,7 +2,8 @@
 if(ENVIRONMENT=="development"){
 	$config['access_ip_list']	= array(
 		'114.34.172.44',
-		'54.64.205.49'
+		'54.64.205.49',
+        '127.0.0.1'
 	);
 
     $config['bank_adapter_ip'] = '54.64.205.49';
@@ -11,7 +12,8 @@ if(ENVIRONMENT=="development"){
 		'114.34.172.44',
 		'13.112.224.83',
         '52.194.4.73',
-        '18.179.183.180'
+        '18.179.183.180',
+        '127.0.0.1'
 	);
     // TODO: 待確認是否為私網IP, ex: 172.xx.xx.xx
     // octopoda ip
