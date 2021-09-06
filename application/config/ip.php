a7e63efb24309a7d0f83406dcd79824a0904fce2
kevin-influx <kevin@influxfin.com>
2021-09-06 18:05:31 +0800

    Delete local config

--- application/config/ip.php
+++ application/config/ip.php
@@ -3,7 +3,6 @@ if(ENVIRONMENT=="development"){
 	$config['access_ip_list']	= array(
 		'114.34.172.44',
 		'54.64.205.49',
-        '127.0.0.1'
 	);
 
     $config['bank_adapter_ip'] = '54.64.205.49';
@@ -13,7 +12,6 @@ if(ENVIRONMENT=="development"){
 		'13.112.224.83',
         '52.194.4.73',
         '18.179.183.180',
-        '127.0.0.1'
 	);
     // TODO: 待確認是否為私網IP, ex: 172.xx.xx.xx
     // octopoda ip
