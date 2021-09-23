b25ab812a526b7dd7118d9ec972a18aecb16cded
kevin-influx <kevin@influxfin.com>
2021-09-23 12:25:30 +0800

    Fix image location
    修正 - 媒體報導圖片位置

--- resources/js/frontdesk/pages/alesis-index.vue
+++ resources/js/frontdesk/pages/alesis-index.vue
@@ -456,15 +456,15 @@
             <alesis-space size="medium"></alesis-space>
             <div class="包裹容器">
                 <div class="商標區塊">
-                    <a class="項目" href="https://tw.money.yahoo.com/%E9%87%91%E8%9E%8D%E7%A7%91%E6%8A%80%E5%89%B5%E6%96%B0%E5%9C%92%E5%8D%809%E5%AE%B6%E5%BB%A0%E5%95%86%E5%8A%A0%E5%85%A5-%E4%BC%81%E6%A5%AD%E5%AF%A6%E9%A9%97%E5%AE%A4%E9%A6%96%E6%B3%A2%E5%85%AD%E5%A4%A7%E4%B8%BB%E9%A1%8C%E5%85%AC%E5%B8%83-083826449.html" target="_blank"><img class="圖片" src="/images/a645f762048f5f425d0f0a8486f34612.png"></a>
-                    <a class="項目" href="http://n.yam.com/Article/20180803346949" target="_blank"><img class="圖片" src="/images/1JHY1QOLYPF0.png"></a>
-                    <a class="項目" href="https://www.nownews.com/news/20180807/2798010/" target="_blank"><img class="圖片" src="/images/Now_News_logo.png"></a>
-                    <a class="項目" href="https://act.chinatimes.com/market/content.aspx?AdID=6585&chdtv" target="_blank"><img class="圖片" src="/images/logo-chinatimes2019-1200x635.png"></a>
+                    <a class="項目" href="https://tw.money.yahoo.com/%E9%87%91%E8%9E%8D%E7%A7%91%E6%8A%80%E5%89%B5%E6%96%B0%E5%9C%92%E5%8D%809%E5%AE%B6%E5%BB%A0%E5%95%86%E5%8A%A0%E5%85%A5-%E4%BC%81%E6%A5%AD%E5%AF%A6%E9%A9%97%E5%AE%A4%E9%A6%96%E6%B3%A2%E5%85%AD%E5%A4%A7%E4%B8%BB%E9%A1%8C%E5%85%AC%E5%B8%83-083826449.html" target="_blank"><img class="圖片" src="/upload/media/a645f762048f5f425d0f0a8486f34612.png"></a>
+                    <a class="項目" href="http://n.yam.com/Article/20180803346949" target="_blank"><img class="圖片" src="/upload/media/1JHY1QOLYPF0.png"></a>
+                    <a class="項目" href="https://www.nownews.com/news/20180807/2798010/" target="_blank"><img class="圖片" src="/upload/media/Now_News_logo.png"></a>
+                    <a class="項目" href="https://act.chinatimes.com/market/content.aspx?AdID=6585&chdtv" target="_blank"><img class="圖片" src="/upload/media/logo-chinatimes2019-1200x635.png"></a>
                 </div>
                 <div class="商標區塊 商標區塊_三欄">
-                    <a class="項目" href="https://news.cnyes.com/news/id/4267004" target="_blank"><img class="圖片" src="/images/logo-cn-yes-1.png"></a>
-                    <a class="項目" href="https://www.wealth.com.tw/home/articles/20567" target="_blank"><img class="圖片" src="/images/logo-red.png"></a>
-                    <a class="項目" href="https://m.ctee.com.tw/livenews/aj/a95645002019042615564198" target="_blank"><img class="圖片" src="/images/logo300_90.png"></a>
+                    <a class="項目" href="https://news.cnyes.com/news/id/4267004" target="_blank"><img class="圖片" src="/upload/media/logo-cn-yes-1.png"></a>
+                    <a class="項目" href="https://www.wealth.com.tw/home/articles/20567" target="_blank"><img class="圖片" src="/upload/media/logo-red.png"></a>
+                    <a class="項目" href="https://m.ctee.com.tw/livenews/aj/a95645002019042615564198" target="_blank"><img class="圖片" src="/upload/media/logo300_90.png"></a>
                 </div>
             </div>
         </div>
