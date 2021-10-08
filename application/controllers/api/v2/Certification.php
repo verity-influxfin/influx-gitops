e9e883a1593d5ea376cd449bca7da21b881319f3
kevin-influx <kevin@influxfin.com>
2021-10-08 10:28:48 +0800

    Fix error

--- application/controllers/api/v2/Certification.php
+++ application/controllers/api/v2/Certification.php
@@ -2242,10 +2242,12 @@ class Certification extends REST_Controller {
             if($this->user_info->company_status == 1){
                 $user_id = '';
                 $investor = '';
-                $this->user_info = $this->user_model->get_by(array( 'phone' => $this->user_info->phone, 'company_status' => 0, 'investor_status' => 0));
-
-                if(!empty($this->user_info)){
-                    $user_id = $this->user_info->id;
+                print_r($this->user_info->phone);exit;
+                $user_info = $this->user_model->get_by(array( 'phone' => $this->user_info->phone, 'company_status' => 0));
+                if(!empty($user_info)){
+                    $user_id = $user_info->id;
+                }else{
+                    $this->response(array('result' => 'ERROR', 'error' => INSERT_ERROR));
                 }
             }
 
