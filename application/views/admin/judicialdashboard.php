7523dca085f29d873947e788a3b5d29b1a09bb63
kevin-influx <kevin@influxfin.com>
2021-10-12 12:15:05 +0800

    Fix dashboard

--- application/views/admin/judicialdashboard.php
+++ application/views/admin/judicialdashboard.php
@@ -89,7 +89,7 @@
                     </div>
                 </div>
             </div>
-            <a target="_parent" href="<? //=admin_url('Target/index?delay=1&status=5') ?>">
+            <a target="_parent" href="<?= admin_url('Target/waiting_verify') ?>">
                 <div class="panel-footer">
                     <span class="pull-left">View Details</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
