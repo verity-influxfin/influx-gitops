63dd3bc3f31d6aa9acaa8c06cd56fa0eb1b7826b
kevin-influx <kevin@influxfin.com>
2021-11-08 16:06:01 +0800

    Fix social of scraper library
    修正 - 社交認證爬蟲函式庫

--- application/libraries/scraper/Instagram_lib.php
+++ application/libraries/scraper/Instagram_lib.php
@@ -6,8 +6,7 @@ class Instagram_lib
     function __construct($params=[])
     {
         $this->CI = &get_instance();
-        $judicialYuanServerPort = '9998';
-        $this->scraperUrl = "http://" . getenv('GRACULA_IP') . ":{$judicialYuanServerPort}/scraper/api/v1.0/instagram_deep/";
+        $this->scraperUrl = "http://" . getenv('GRACULA_IP') . ":".getenv('GRACULA_PORT')."/scraper/api/v1.0/instagram/";
         if(isset($params['ip'])){
           $this->scraperUrl = "http://{$params['ip']}/scraper/api/v1.0/";
         }
@@ -39,7 +38,7 @@ class Instagram_lib
 
         $url = $this->scraperUrl  . "{$reference}/{$followed_account}/info";
         $result = curl_get($url);
-        $response = json_decode($result);
+        $response = json_decode($result,true);
 
         if (!$result || !isset($response->status)) {
             return false;
@@ -57,9 +56,59 @@ class Instagram_lib
         $url = $this->scraperUrl  . "{$reference}/{$followed_account}/info";
         $data = ['key'=>''];
         $result = curl_get($url, $data);
-        $response = json_decode($result);
+        $response = json_decode($result,true);
 
-        if (!$result || !isset($response->status) || $response->status != 200) {
+        if (!$result || !isset($response->status)) {
+            return false;
+        }
+
+        return $response;
+    }
+
+    public function getLogStatus($reference, $followed_account)
+    {
+        if(!$followed_account || !$reference) {
+            return false;
+        }
+
+        $url = $this->scraperUrl  . "{$reference}/{$followed_account}/taskLog";
+        $result = curl_get($url);
+        $response = json_decode($result.true);
+
+        if (!$result || !isset($response->status)) {
+            return false;
+        }
+
+        return $response;
+    }
+
+    public function getRiskControlInfo($reference, $followed_account){
+        if(!$followed_account || !$reference) {
+            return false;
+        }
+
+        $url = $this->scraperUrl  . "{$reference}/{$followed_account}/riskControlInfo";
+        $result = curl_get($url);
+        $response = json_decode($result,true);
+
+        if (!$result || !isset($response->status)) {
+            return false;
+        }
+
+        return $response;
+    }
+
+    public function updateRiskControlInfo($reference, $followed_account){
+        if(!$followed_account || !$reference) {
+            return false;
+        }
+
+        $url = $this->scraperUrl  . "{$reference}/{$followed_account}/riskControlInfo";
+        $data = ['key'=>''];
+        $result = curl_get($url);
+        $response = json_decode($result,true);
+
+        if (!$result || !isset($response->status)) {
             return false;
         }
 
