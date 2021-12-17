878dc062a4290eb02324947a883b4322d372f4d6
allan-influx <allan@influxfin.com>
2021-12-17 11:54:28 +0800

    fix: 加入&預設網頁字體為 Noto Sans TC

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,6 +31,7 @@
     <link rel="stylesheet" href="{{ asset('css/package/pagination.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/package/hover-min.css') }}">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+TC">
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <link
     rel="stylesheet"
