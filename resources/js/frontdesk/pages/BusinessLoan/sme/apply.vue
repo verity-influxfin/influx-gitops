5b12bd50fa13463f91c26986a515ff980c7bc0d9
allan-influx <allan@influxfin.com>
2021-12-22 21:24:59 +0800

    feature: 新的表單樣式

--- resources/js/frontdesk/pages/BusinessLoan/sme/apply.vue
+++ resources/js/frontdesk/pages/BusinessLoan/sme/apply.vue
@@ -9,7 +9,7 @@
     ></button>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <label class="input-group-text">統一編號</label>
+        <label class="input-group-text">1.統一編號</label>
       </div>
       <input
         type="text"
@@ -20,25 +20,25 @@
     </div>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <label class="input-group-text">公司名稱</label>
+        <label class="input-group-text">2.公司名稱</label>
       </div>
       <input type="text" class="form-control" v-model="company_name" />
     </div>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <label class="input-group-text">聯絡人</label>
+        <label class="input-group-text">3.聯絡人</label>
       </div>
       <input type="text" class="form-control" v-model="contact_person" />
     </div>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <label class="input-group-text">聯絡電話</label>
+        <label class="input-group-text">4.聯絡電話</label>
       </div>
       <input type="tel" class="form-control" v-model="contact_phone" />
     </div>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <label class="input-group-text">電子信箱</label>
+        <label class="input-group-text">5.電子信箱</label>
       </div>
       <input type="email" class="form-control" v-model="email" />
     </div>
@@ -94,7 +94,7 @@ export default {
       if (tax_id.length > 7) {
         axios({
           method: 'get',
-          url: '/api/v1/getCompanyName?tax_id='+tax_id,
+          url: '/api/v1/getCompanyName?tax_id=' + tax_id,
         }).then(({ data }) => {
           if (data?.data) {
             this.company_name = data?.data
@@ -113,6 +113,14 @@ export default {
   max-width: 900px;
   .input-group-text {
     width: 100px;
+    background-color: #fff;
+    border: none;
+  }
+  .btn-block {
+    margin-left: 100px;
+    width: calc(100% - 100px);
+    background-color: #326398;
+    color: #fff;
   }
 }
 @media screen and (max-width: 767px) {
@@ -121,6 +129,10 @@ export default {
     max-width: min-content;
     .input-group-text {
       width: 85vw;
+    }
+    .btn-block {
+      margin-left: 0;
+      width: 100%;
     }
   }
 }
