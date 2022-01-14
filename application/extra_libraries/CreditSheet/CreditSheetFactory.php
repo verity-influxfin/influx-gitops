63bfe400df6ebc923ebbc280599f599ecd98f8a8
kevin-influx <kevin@influxfin.com>
2022-01-14 11:14:27 +0800

    Feature call credit sheet object logic
    新增 - 授審表判斷法人與自然人

--- application/extra_libraries/CreditSheet/CreditSheetFactory.php
+++ application/extra_libraries/CreditSheet/CreditSheetFactory.php
@@ -19,21 +19,50 @@ class CreditSheetFactory {
         $user = $CI->user_model->get_by(['id'=> $target->user_id ?? 0]);
         $creditSheetRecord = $CI->credit_sheet_model->get_by(['target_id' => $target->id ?? 0, 'status' => 1]);
 
+        // 判斷類型
+        $call_type = '';
+        if (in_array($target->product_id,['1002']))
+        {
+            $call_type = TYPE_COMPANY;
+        }
+        elseif (in_array($target->product_id,['1','2','3','4']))
+        {
+            $call_type = TYPE_PERSONAL;
+        }
+
         if(isset($creditSheetRecord)) {
             // 已封存之個金授審表
-            $basicInfo = new ArchivingPersonalBasicInfo();
-            $creditLineInfo = new ArchivingCreditLineInfo();
-            $cashLoanInfo = new ArchivingCashLoanInfo();
+            if ($call_type == TYPE_PERSONAL)
+            {
+                $basicInfo = new ArchivingPersonalBasicInfo();
+                $creditLineInfo = new ArchivingCreditLineInfo();
+                $cashLoanInfo = new ArchivingCashLoanInfo();
+            }
+            elseif ($call_type == TYPE_COMPANY)
+            {
+            }
         }else{
             // 未封存之個金授審表
-            $basicInfo = new PersonalBasicInfo();
-            $creditLineInfo = new CreditLineInfo();
-            $cashLoanInfo = new CashLoanInfo();
+            if ($call_type == TYPE_PERSONAL)
+            {
+                $basicInfo = new PersonalBasicInfo();
+                $creditLineInfo = new CreditLineInfo();
+                $cashLoanInfo = new CashLoanInfo();
+            }
+            elseif ($call_type == TYPE_COMPANY)
+            {
+            }
         }
 
         $returnObject = NULL;
         try{
-            $returnObject = new PersonalCreditSheet($target, $user, $basicInfo, $creditLineInfo, $cashLoanInfo);
+            if ($call_type == TYPE_PERSONAL)
+            {
+                $returnObject = new PersonalCreditSheet($target, $user, $basicInfo, $creditLineInfo, $cashLoanInfo);
+            }
+            elseif ($call_type == TYPE_COMPANY)
+            {
+            }
         }catch (\InvalidArgumentException $e) {
             error_log("Invalid Argument Exception: ". $e->getMessage());
         }catch (\Exception $e) {
