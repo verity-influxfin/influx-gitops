46214a806472e9850a80ac5d24f15a1c719c1868
Jack Jang <jack@influxfin.com>
2022-02-11 04:32:36 +0800

    feature: 電子看板暨疊床架屋粉飾太平大王兼操盤手

---
+++ application/controllers/Page.php
@@ -0,0 +1,186 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+use GuzzleHttp\Client;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Ecdsa\Sha256;
+
+class Page extends CI_Controller
+{
+    public function eboard()
+    {
+        $this->load->view('eboard_page');
+    }
+
+    public function get_eboard_data()
+    {
+        $retval = [];
+        $first_day = (new DateTimeImmutable())->modify('- 6 day');
+
+
+        for ($i=0; $i<7; $i++)
+        {
+            $date = $i > 0 ? $first_day->modify("+ {$i} day") : $first_day;
+
+            $retval[] = [
+
+                // 日期
+                'date'                 => $tx_date = $date->format('Y-m-d'),
+
+                // 官網流量
+                'official_site_trends' => 0,
+
+                // 新增會員
+                'new_member'           => $this->_get_new_member($date),
+
+                // 會員總數
+                'total_member'         => $this->_get_total_member($date),
+
+                // APP下載
+                'app_downloads'        => 0,
+
+                // 各產品每月申貸數
+                'product_bids'         => 0,
+
+                // 成交
+                'deals'                => $this->_get_deals($date)
+            ];
+        }
+
+        usort($retval, function ($a, $b) {
+            return $b['date'] <=> $a['date'];
+        });
+
+        $this->output->set_content_type('application/json')
+                    ->set_output(json_encode([
+                        'result' => 'success',
+                        'data'   => $retval
+                    ]));
+    }
+
+    private function _get_deals(DateTimeInterface $date)
+    {
+        $this->load->model('loan/target_model');
+        $query = $this->target_model->db->select([
+                                            'COUNT(*) AS amount',
+                                            'loan_date'
+                                        ])->from('p2p_loan.targets')
+                                        ->where_in('status', [5, 10])
+                                        ->where([
+                                            'loan_status' => 1,
+                                            'loan_date'   => $date->format('Y-m-d')
+                                        ])
+                                        ->group_by('loan_date')
+                                        ->get()
+                                        ->first_row('array');
+        return $query['amount'] ?? 0;
+    }
+
+    private function _get_total_member(DateTimeInterface $date)
+    {
+
+        $this->load->model('user/user_model');
+
+        $unixtime_query = sprintf('FROM_UNIXTIME(created_at, \'%s\')', $date->format('Y m d'));
+        
+        $query = $this->user_model->db->select('COUNT(id) AS amount')
+                                ->select($unixtime_query . ' AS date')
+                                ->from('p2p_user.users')
+                                ->where([
+                                    'status'        => 1
+                                ])
+                                ->group_by($unixtime_query)
+                                ->get()
+                                ->first_row('array');
+        return $query['amount'] ?? 0;
+    }
+
+    private function _get_new_member(DateTimeInterface $date)
+    {
+
+        $this->load->model('user/user_model');
+
+        $unixtime_query = sprintf('FROM_UNIXTIME(created_at, \'%s\')', $date->format('Y m d'));
+        
+        $query = $this->user_model->db->select('COUNT(id) AS amount')
+                                ->select($unixtime_query . ' AS date')
+                                ->from('p2p_user.users')
+                                ->where([
+                                    'created_at >=' => $date->getTimestamp(),
+                                    'created_at <'  => $date->modify('+1 day')->getTimestamp(),
+                                    'status'        => 1
+                                ])
+                                ->group_by($unixtime_query)
+                                ->get()
+                                ->first_row('array');
+        return $query['amount'] ?? 0;
+    }
+
+    private function _get_ios_sales_summary_data()
+    {
+        $ch = curl_init();
+
+        $url = 'https://api.appstoreconnect.apple.com/v1/salesReports?filter[frequency]=DAILY&filter[reportSubType]=SUMMARY&filter[reportType]=SALES&filter[vendorNumber]=88313024&filter[version]=1_0';
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept-Encoding: gzip, deflate, br',
+            'Authorization: Bearer ' . $this->_get_app_store_connect_api_token()
+        ]);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        $text = gzdecode($result);
+
+        $matrix = array_map(function ($row) {
+            return explode("\t", $row);
+        }, array_filter(
+            explode(PHP_EOL, $text),
+            function ($row) { return ! empty($row); }
+        ));
+    }
+
+    private function _get_app_store_connect_api_token()
+    {
+        $this->load->driver('cache', [
+            'adapter' => 'apc',
+            'backup'  => 'file'
+        ]);
+
+        $key = 'app_store_connect_api_token';
+
+        if ( ! $token = $this->cache->get($key))
+        {
+                $token = $this->_generate_app_store_connect_api_token();
+
+                // 存放 5 分鐘
+                $this->cache->save($key, $token, 1200);
+        }
+        return $token;
+    }
+
+    private function _generate_app_store_connect_api_token()
+    {
+        $time   = new DateTimeImmutable();
+        $signer = new Sha256();
+        $token  = new Builder();
+
+        $token->setHeader('alg','ES256');
+        $token->setHeader('typ','JWT');
+        $token->setHeader('kid','7RU6XMTDHZ');
+
+        $token->set('iss','d1131f2b-f0a2-4e6a-b7c8-3ec40137057a');
+        $token->set('iat', $time);
+        $token->set('exp', $time->modify( '+20 min' ));
+        $token->set('aud','appstoreconnect-v1');
+
+        return $token->getToken($signer, new Key('-----BEGIN PRIVATE KEY-----
+MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgST75E7HzthaY4Xs2
+Y6JOXMA7m9EdhbfPZEsCpskCOPugCgYIKoZIzj0DAQehRANCAAR/W+y0k9sqr4kH
+7FiAXKYlA0K/5f/fYj2f2KEme+eIfq7RCqdqTtV7V2oMg93EDYXCgBoPDCRCxzP8
+VhvEDNkC
+-----END PRIVATE KEY-----'));
+    }
+}
---
+++ application/views/eboard_page.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="zh-TW">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>電子看板</title>
+    </head>
+    <body id="app">
+        <table class="table table-dark table-striped h-100 w-100" v-if="state.data">
+            <thead>
+                <tr>
+                    <th>日期</th>
+                    <th>官網流量</th>
+                    <th>新增會員</th>
+                    <th>會員總數</th>
+                    <th>APP下載</th>
+                    <th>各產品每月申貸數</th>
+                    <th>成交</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in state.data">
+                    <th v-text="item.date"></th>
+                    <td v-text="item.official_site_trends"></td>
+                    <td v-text="item.new_member"></td>
+                    <td v-text="item.total_member"></td>
+                    <td v-text="item.app_downloads"></td>
+                    <td v-text="item.product_bids"></td>
+                    <td v-text="item.deals"></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/vue@3"></script>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script>
+            const { reactive, onBeforeMount } = Vue;
+
+            Vue.createApp({
+              data() {
+                return {
+                  data: []
+                }
+              },
+              setup(){
+                  const state = reactive({
+                      data: null,
+                  })
+                  onBeforeMount(() => {
+                      axios.get("/page/get_eboard_data").then(function (response) {
+                          state.data = response.data.data;
+                      })
+                  })
+                  return { state };
+              }
+            }).mount('#app')
+        </script>
+    </body>
+</html>
