9ed3e8b66a650530eaf23bba7eebf09e30e6cfea
wayne-influx <wayne@influxfin.com>
2022-02-17 17:14:01 +0800

    Fix - 調整上班族貸每案皆需進入「二審」 #1833

--- application/libraries/Target_lib.php
+++ application/libraries/Target_lib.php
@@ -359,7 +359,6 @@ class Target_lib
                         //檢核產品額度，不得高於個人最高歸戶剩餘額度
                         $credit['amount'] = $used_amount > $user_current_credit_amount ? $user_current_credit_amount : $used_amount;
 
-
                         $loan_amount = $target->amount > $credit['amount'] && $subloan_status == false ? $credit['amount'] : $target->amount;
                         // 金額取整程式，2020/10/30排除產轉
                         $loan_amount = ($loan_amount % 1000 != 0 && $subloan_status == false) ? floor($loan_amount * 0.001) * 1000 : $loan_amount;
@@ -381,6 +380,8 @@ class Target_lib
                                     && !$this->CI->anti_fraud_lib->judicialyuan($target->user_id)
                                     && $this->judicialyuan($user_id)
                                     && $target->product_id < 1000 && $target->sub_status != TARGET_SUBSTATUS_SECOND_INSTANCE
+                                    // 依照產品部門需求，上班族暫時全部強制進待二審
+                                    && ! in_array($target->product_id, [PRODUCT_ID_SALARY_MAN, PRODUCT_ID_SALARY_MAN_ORDER])
                                     || $subloan_status
                                     || $renew
                                     || $evaluation_status
