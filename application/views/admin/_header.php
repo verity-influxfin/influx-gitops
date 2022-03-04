5ad14504a301c2ce6a304875988621abe85d144e
allan-influx <allan@influxfin.com>
2022-03-04 14:43:24 +0800

    feature: vuejs功能在dev跟prod分別引入不同的版本

--- application/views/admin/_header.php
+++ application/views/admin/_header.php
@@ -42,7 +42,13 @@
     <script data-pace-options='{ "ajax": true }' src="/assets/admin/scripts/pace.js"></script>
 
     <?php if ($use_vuejs ?? false):?>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
+	<?php if (ENVIRONMENT === 'development'):?>
+	<!-- dev -->
+	<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+	<?php else:?>
+	<!-- prod -->
+	<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
+	<?php endif?>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="/assets/admin/js/vue-components.js"></script>
