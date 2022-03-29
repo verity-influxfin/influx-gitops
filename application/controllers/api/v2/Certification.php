1822e974583ae60a4058d52a940c8644b6db33de
許雲輔 <nabroux@xuyunfudeMacBook-Pro.local>
2022-03-29 17:13:03 +0800

    Fix - 將緊急聯絡人不能填寫會員手機改為用戶本身手機 #1969

--- application/controllers/api/v2/Certification.php
+++ application/controllers/api/v2/Certification.php
@@ -1247,6 +1247,7 @@ class Certification extends REST_Controller {
 
                 $phone_exist = $this->user_model->get_by([
                     'phone'		=> $content['phone'],
+                    'id'        => $user_id,
                     'status'	=> 1,
                 ]);
                 if($phone_exist){
