d5ed679c0319616b7416ef723b2c5c593e172cc5
willy-influx <willy@influxfin.com>
2022-04-26 13:59:27 +0800

    feat: 企業評分表 查看評分的頁面

---
+++ application/views/admin/enterprise_credit_sheet.php
@@ -0,0 +1,111 @@
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">企業評分表</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <span>標的資訊</span>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="col-lg-12">
+                            <h3>xxx 股份有限公司</h3>
+                            <hr style="border:1px solid gray;"/>
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" style="font-weight:bold;font-size:16px;" width="100%">
+                                    <tr><td>產業類別</td><td>製造業</td></tr>
+                                </table>
+                                <table class="table table-bordered table-hover" width="100%">
+                                    <thead>
+                                        <tr>
+                                            <th>項目</th>
+                                            <th>說明</th>
+                                            <th>評分標準</th>
+                                            <th>得分</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>產業要素</td>
+                                            <td>產業生命週期</td>
+                                            <td>成長期</td>
+                                            <td>10</td>
+                                        </tr>
+                                        <tr>
+                                            <td>借戶要素</td>
+                                            <td>企業經營資歷 經濟部公司設立年資</td>
+                                            <td>2年</td>
+                                            <td>6</td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td>實收資本額(經濟部登記資訊)</td>
+                                            <td>7,500,000</td>
+                                            <td>4</td>
+                                        </tr>
+                                        <tr>
+                                            <td>經營效能</td>
+                                            <td>...</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td>...</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td>...</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td>...</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>財務要素</td>
+                                            <td>...</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>保證要素</td>
+                                            <td>...</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>DD查核</td>
+                                            <td>...</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                                <table class="table table-bordered" style="font-weight:bold;font-size:16px;" width="100%">
+                                    <tr><td>總分</td><td>999999</td></tr>
+                                </table>
+
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+        </div>
+        <!-- /#page-wrapper -->
