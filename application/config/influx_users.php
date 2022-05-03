2722bce5a1c1dc7de71502042efb8da452e7543e
joanne <joanne@influxfin.com>
2022-05-03 10:44:46 +0800

    issue: +李奕伽 +王柏勝 +劉政國 +汪芸瑄 +蔡泓霖 -Steven -Jack -俊儒 -意雯 -Roger #2061

--- application/config/influx_users.php
+++ application/config/influx_users.php
@@ -6,20 +6,14 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['influx_user_list'] = [
-    [ // Roger
-        'user_id' => 59],
     [ // News
         'user_id' => 82],
     [ // Arthur
         'user_id' => 88],
     [ // Mori
         'user_id' => 99],
-    [ // 意雯
-        'user_id' => 12614],
     [ // 蔡蔡
         'user_id' => 23643],
-    [ // 俊儒
-        'user_id' => 23645],
     [ // Alex
         'user_id' => 41777],
     [ // Frankie
@@ -32,14 +26,10 @@ $config['influx_user_list'] = [
         'user_id' => 56168],
     [ // Wayne
         'user_id' => 61858],
-    [ // Jack
-        'user_id' => 66389],
     [ // Joanne
         'user_id' => 70661],
     [ // Allan
         'user_id' => 72774],
-    [ // Steven
-        'user_id' => 76490],
     [ // Peter
         'user_id' => 76574],
     [ // Willy
@@ -48,4 +38,14 @@ $config['influx_user_list'] = [
         'user_id' => 79054],
     [ // 雪玉
         'user_id' => 79329],
+    [ // 李奕伽
+        'user_id' => 83929],
+    [ // 王柏勝
+        'user_id' => 83340],
+    [ // 劉政國
+        'user_id' => 83529],
+    [ // 汪芸瑄
+        'user_id' => 83848],
+    [ // 蔡泓霖
+        'user_id' => 51365],
 ];
