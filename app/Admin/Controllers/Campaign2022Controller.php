1b0fac547deaa1bcf7eb97de7c5c14091e716758
Joanne <joanne@influxfin.com>
2022-05-31 17:45:54 +0800

    feature: 後台灌票系統，輸入單日總票數由系統自動配票

--- app/Admin/Controllers/Campaign2022Controller.php
+++ app/Admin/Controllers/Campaign2022Controller.php
@@ -9,6 +9,7 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
+use Illuminate\Support\MessageBag;
 
 class Campaign2022Controller extends Controller
 {
@@ -82,7 +83,7 @@ class Campaign2022Controller extends Controller
     {
         $grid = new Grid(new Campaign2022_add);
 
-        $grid->model()->orderBy('id', 'desc');
+        $grid->model()->orderBy('created_at', 'desc');
 
         // 關閉選擇器
         $grid->disableRowSelector();
@@ -161,7 +162,45 @@ class Campaign2022Controller extends Controller
 
         });
         $form->number('campaign2022s_id', '活動作品id')->required()->default(18)->rules('integer', ['integer' => '請輸入正確的作品id']);
-        $form->number('votes', '票數')->required()->rules('min:0|integer', ['min' => '請輸入大於0的整數', 'integer' => '請輸入整數']);
+        $form->number('votes', '總票數')->required()->rules('min:0|integer', ['min' => '請輸入大於0的整數', 'integer' => '請輸入整數']);
+
+        $now = (new \DateTime())->setTimezone(new \DateTimeZone('+8'))->getTimestamp();
+        $form->date('date', '灌票日期')->required()->default(date('Y-m-d', $now));
+
+        // 表單儲存前的處理
+        $form->saving(function (Form $form) use ($now) {
+            $total_votes = $form->votes;
+            $date = (new \DateTime())
+                ->setTimezone(new \DateTimeZone('+8'))
+                ->setDate(substr($form->date, 0, 4), substr($form->date, 5, 2), substr($form->date, 8, 2));
+
+            if ($now > $date->getTimestamp()) {
+                $error = new MessageBag([
+                    'title' => '灌票日期不可小於今日',
+                ]);
+
+                return back()->with(compact('error'));
+            }
+
+            while ($total_votes > 0) {
+                $votes = min($total_votes, rand(1, 5));
+                $total_votes -= $votes;
+
+                $start_time = max($now, $date->setTime(0, 0)->getTimestamp());
+                $end_time = $date->setTime(23, 59, 59)->getTimestamp();
+
+                $created_at = date('Y-m-d H:i:s', rand($start_time, $end_time));
+                $form_data = new Campaign2022_add([
+                    'campaign2022s_id' => (int)$form->campaign2022s_id,
+                    'votes' => $votes
+                ]);
+                $form_data->created_at = $created_at;
+                $form_data->updated_at = $created_at;
+                $form_data->save();
+            }
+            exit;
+        });
+
         return $form;
     }
 }
