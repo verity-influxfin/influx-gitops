1b878e76c5a8e78d505cb5fe3925532eebdf22b5
allan-influx <allan@influxfin.com>
2022-06-02 11:07:52 +0800

    feature: 加上案件總覽story 及 拆分案件總覽資料及UI

---
+++ resources/js/frontdesk/component/enterpriseUpload/pureCaseOverview.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="case-overview">
+    <div class="case-overview-title">
+      {{ caseOverview.title }}
+    </div>
+    <div class="case-overview-item">產品：{{ caseOverview.productTitle }}</div>
+    <div class="case-overview-item">案件編號：{{ caseOverview.caseNum }}</div>
+    <div class="case-overview-item">申貸日期：{{ caseOverview.startDate }}</div>
+    <div class="case-overview-item">
+      申貸金額：${{ caseOverview.loanMoney }}
+    </div>
+    <div class="case-overview-item">借款期間：{{ caseOverview.loanDur }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    caseOverview: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.case-overview {
+  padding: 30px;
+  background: #ffffff;
+  border-radius: 20px;
+  .case-overview-title {
+    color: #393939;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 35px;
+    margin-bottom: 15px;
+  }
+  .case-overview-item {
+    color: #707070;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 26px;
+  }
+}
+</style>
