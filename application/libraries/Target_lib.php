a92e182848f453d6686579279a703b2a438c20ab
wayne-influx <wayne@influxfin.com>
2022-06-14 14:28:58 +0800

    Fix: 移除階段上架錯誤的核准額度判斷 #2158

--- application/libraries/Target_lib.php
+++ application/libraries/Target_lib.php
@@ -356,7 +356,7 @@ class Target_lib
                         $loan_amount = $target->amount > $credit['amount'] && $subloan_status == false ? $credit['amount'] : $target->amount;
                         // 金額取整程式，2020/10/30排除產轉
                         $loan_amount = ($loan_amount % 1000 != 0 && $subloan_status == false) ? floor($loan_amount * 0.001) * 1000 : $loan_amount;
-                        if ($loan_amount >= $product_info['loan_range_s'] || $subloan_status || $stage_cer != 0 && $loan_amount >= STAGE_CER_MIN_AMOUNT) {
+                        if ($loan_amount >= $product_info['loan_range_s'] || $subloan_status) {
                             if ($product_info['type'] == 1 || $subloan_status) {
                                 $platform_fee = $this->CI->financial_lib->get_platform_fee($loan_amount, $product_info['charge_platform']);
                                 $param = [
