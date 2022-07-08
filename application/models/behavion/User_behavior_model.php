2445df295ad074a475d0e4c96876e74cefac6514
wayne-influx <wayne@influxfin.com>
2022-07-08 09:41:30 +0800

    Fix: 修正推薦碼取得首次登入數量的邏輯

--- application/models/behavion/User_behavior_model.php
+++ application/models/behavion/User_behavior_model.php
@@ -22,9 +22,12 @@ class User_behavior_model extends MY_Model
     /**
      * 依照推薦碼取得首次登入數量
      * @param $promoteCode
+     * @param $startTime
+     * @param $endTime
+     * @param bool $group_by_code
      * @return array
      */
-    public function getFirstOpenCountByPromoteCode($promoteCode, $startTime, $endTime, $is_group_by_code = TRUE): array
+    public function getFirstOpenCountByPromoteCode($promoteCode, $startTime, $endTime, bool $group_by_code = TRUE): array
     {
 	    if(empty($promoteCode))
 	        return [];
@@ -41,16 +44,16 @@ class User_behavior_model extends MY_Model
 
         $subQuery = $this->db->get_compiled_select('', TRUE);
         $this->db
-            ->select('uq.id AS user_qrcode_id, r.promote_code, r.created_at, COUNT(r.promote_code) as count')
+            ->select('uq.id AS user_qrcode_id, r.promote_code, r.created_at')
             ->from('`p2p_user`.`user_qrcode` AS `uq`')
             ->join("($subQuery) as `r`", "`r`.`promote_code` = `uq`.`promote_code`")
             ->where('`uq`.`status`', 1)
             ->where('`r`.`created_at` >= `uq`.`start_time`')
             ->where('`r`.`created_at` < `uq`.`end_time`');
 
-        if ($is_group_by_code)
+        if ($group_by_code)
         {
-            $this->db->select('COUNT(r.promote_code) as count')
+            $this->db->select('COUNT(1) as count')
                 ->group_by('`r`.`promote_code`');
         }
         else
