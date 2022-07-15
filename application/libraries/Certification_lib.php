671f4f77622c2679965ae3af7526304dced66b5b
Joanne <joanne@influxfin.com>
2022-07-15 15:26:59 +0800

    fix: 移除誤加的徵信跑批判斷

--- application/libraries/Certification_lib.php
+++ application/libraries/Certification_lib.php
@@ -3105,8 +3105,7 @@ class Certification_lib{
 		$ids			= array();
 		$user_certifications 	= $this->CI->user_certification_model->order_by('certification_id','ASC')->get_many_by(array(
 			'status'				=> 0,
-			'certification_id !='	=> 3,
-            'id'=>1007778
+			'certification_id !='	=> 3
 		));
 		if($user_certifications){
 			foreach($user_certifications as $key => $value){
