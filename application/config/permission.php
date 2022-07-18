6e3950716e5253e28115470b2391d26f6c9a366e
Joanne <joanne@influxfin.com>
2022-07-18 15:58:15 +0800

    fix: 修改權限，虛擬帳號的 detail

--- application/config/permission.php
+++ application/config/permission.php
@@ -173,6 +173,7 @@ $config['permission'] = [
         'permission' => [
             'user_bankaccount_list' => ['model' => 'Passbook', 'submodel' => 'user_bankaccount_list', 'action' => 'read'],
             'index' => ['model' => 'Passbook', 'submodel' => 'index', 'action' => 'read'],
+            'detail' => ['model' => 'Passbook', 'submodel' => 'index', 'action' => 'read'],
             'withdraw_list' => ['model' => 'Passbook', 'submodel' => 'withdraw_list', 'action' => 'read'],
             'withdraw_waiting' => ['model' => 'Passbook', 'submodel' => 'withdraw_waiting', 'action' => 'read'],
             'unknown_funds' => ['model' => 'Passbook', 'submodel' => 'unknown_funds', 'action' => 'read'],
