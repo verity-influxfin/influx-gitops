6cab3beff6d49f0881935988e98463c9ff17a521
Joanne <joanne@influxfin.com>
2022-07-19 15:23:20 +0800

    fix: 修正解衝突遺留的問題

--- application/controllers/api/v2/Product.php
+++ application/controllers/api/v2/Product.php
@@ -1570,6 +1570,10 @@ class Product extends REST_Controller {
 					}
                     $diploma = $key==8?$value:null;
                     if(in_array($key,$product['certifications']) && $value['id'] != CERTIFICATION_CERCREDITJUDICIAL){
+                        $truly_failed = certification_truly_failed($exist_target_submitted, $value['certification_id'] ?? 0,
+                            USER_BORROWER,
+                            is_judicial_product($target->product_id)
+                        );
                         if (in_array($key, $skip_certification_ids))
                         {
                             $value['user_status'] = CERTIFICATION_STATUS_SUCCEED;
