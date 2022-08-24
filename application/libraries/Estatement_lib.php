e64fdd2d1ce62f9711cbc8f48159259eb9d8c3b8
James Ho <jamesho@influxfin.com>
2022-08-24 14:57:31 +0800

    fix: create estatement content on the first day of every month instead of the 10th day #2415
    - 交易對帳單 will be sent on the first day of every month as a result.
        - The content will be records of the whole previous month.

--- application/libraries/Estatement_lib.php
+++ application/libraries/Estatement_lib.php
@@ -1069,7 +1069,7 @@ class Estatement_lib{
 	}
 
 	function script_create_estatement_content(){
-		$day 				= REPAYMENT_DAY + 1;
+		$day 				= 1;  // Create estatement content on the first day of every month.
 		$count				= 0;
 		$entering_date		= get_entering_date();
 		$date  				= date("Y-m-j",strtotime($entering_date));
