fdba7df9734a2a628ec3fb14ac7d6c09ee644213
Joanne <joanne@influxfin.com>
2022-10-25 14:11:35 +0800

    fix: 補徵信項OCR辨識實例入口

--- application/extra_libraries/Certification_ocr/Parser/Ocr_parser_factory.php
+++ application/extra_libraries/Certification_ocr/Parser/Ocr_parser_factory.php
@@ -19,6 +19,14 @@ class Ocr_parser_factory
                 return new Cert_investigation($info);
             case CERTIFICATION_JOB:
                 return new Cert_job($info);
+            case CERTIFICATION_BUSINESSTAX:
+                return new Cert_businesstax($info);
+            case CERTIFICATION_BALANCESHEET:
+                return new Cert_balancesheet($info);
+            case CERTIFICATION_INCOMESTATEMENT:
+                return new Cert_incomestatement($info);
+            case CERTIFICATION_GOVERNMENTAUTHORITIES:
+                return new Cert_governmentauthorities($info);
             default:
                 log_msg('error', "欲建立未支援的認證徵信 OCR Parser 項目 (認證編號:{$info['certification_id']}) ");
                 return NULL;
