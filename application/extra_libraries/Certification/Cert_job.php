b2025b62effac5fc47a9f5de8276335c9335ddc9
Joanne <joanne@influxfin.com>
2023-02-15 15:27:54 +0800

    fix: 調整工作收入證明徵信項，優質企業評分邏輯

--- application/extra_libraries/Certification/Cert_job.php
+++ application/extra_libraries/Certification/Cert_job.php
@@ -237,26 +237,10 @@ class Cert_job extends Cert_pdf
         ];
 
         $this->CI->load->library('credit_lib');
-        $job_company_world_500_point = $this->CI->credit_lib->get_job_company_in_world_500($data['job_company']);
-        $job_company_taiwan_1000_point = $this->CI->credit_lib->get_job_company_in_taiwan_1000($data['job_company']);
-        $job_company_public_agency_point = $this->CI->credit_lib->get_job_company_in_public_agency($data['job_company']);
-        $job_company_medical_institute_point = $this->CI->credit_lib->get_job_company_in_medical_institute($data['job_company']);
-        if (is_numeric($job_company_world_500_point))
-        {
-            $data['job_company_world_500_point'] = $job_company_world_500_point;
-        }
-        if (is_numeric($job_company_taiwan_1000_point))
-        {
-            $data['job_company_taiwan_1000_point'] = $job_company_taiwan_1000_point;
-        }
-        if (is_numeric($job_company_public_agency_point))
-        {
-            $data['job_company_public_agency_point'] = $job_company_public_agency_point;
-        }
-        if (is_numeric($job_company_medical_institute_point))
-        {
-            $data['job_company_medical_institute_point'] = $job_company_medical_institute_point;
-        }
+        $data['job_company_world_500_point'] = $this->CI->credit_lib->get_job_company_in_world_500($data['job_company']);
+        $data['job_company_taiwan_1000_point'] = $this->CI->credit_lib->get_job_company_in_taiwan_1000($data['job_company']);
+        $data['job_company_public_agency_point'] = $this->CI->credit_lib->get_job_company_in_public_agency($data['job_company']);
+        $data['job_company_medical_institute_point'] = $this->CI->credit_lib->get_job_company_in_medical_institute($data['job_company']);
 
         $this->CI->certification_lib->user_meta_progress($data, $this->certification);
 
