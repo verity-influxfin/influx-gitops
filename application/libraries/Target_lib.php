cbcb48eed89a92dc9e9e7ff520c72dd9c5cb0b1f
Joanne <joanne@influxfin.com>
2023-05-18 15:51:31 +0800

    fix: 之前給過但被蓋掉，重給一次預設

--- application/libraries/Target_lib.php
+++ application/libraries/Target_lib.php
@@ -2518,7 +2518,7 @@ class Target_lib
             'charge_platform' => $sub_product['charge_platform'],
             'charge_platform_min' => $sub_product['charge_platform_min'],
             'certifications' => $sub_product['certifications'],
-            'option_certifications' => $sub_product['option_certifications'],
+            'option_certifications' => $sub_product['option_certifications'] ?? [],
             'backend_option_certifications' => $sub_product['backend_option_certifications'],
             'instalment' => $sub_product['instalment'],
             'repayment' => $sub_product['repayment'],
