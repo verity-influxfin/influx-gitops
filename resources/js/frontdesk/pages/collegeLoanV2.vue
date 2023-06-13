693552e52b9d6b5632b08ca77cc9ea9e6a4a5b0b
Derek <root@LAPTOP-5LMCTULN.localdomain>
2023-06-13 13:22:10 +0800

    refactor: workloan & colleage loan add product summary

--- resources/js/frontdesk/pages/collegeLoanV2.vue
+++ resources/js/frontdesk/pages/collegeLoanV2.vue
@@ -581,6 +581,15 @@
         </div>
       </div>
     </div>
+    <div class="product-info">
+      <div class="row no-gutters" style="justify-content: center; height: 50px;">
+        <span style="margin: auto 0; font-size: 16px; color: #023D64; font-weight: 700;">學生貸</span>
+        <span class="ml-3" style="margin: auto 0; font-size: 14px; color: #023D64; opacity: 0.56; font-weight: 400;">•申貸金額：NT$ 3,000~150,000元</span>
+        <span class="ml-3" style="margin: auto 0; font-size: 14px; color: #023D64; opacity: 0.56; font-weight: 400;">•利率：學生貸4%~16%</span>
+        <span class="ml-3" style="margin: auto 0; font-size: 14px; color: #023D64; opacity: 0.56; font-weight: 400;">•期數：3~24期</span>
+        <span class="ml-3" style="margin: auto 0; font-size: 14px; color: #023D64; opacity: 0.56; font-weight: 400;">•平台服務費：平台服務費最低500元，學生貸3%</span>
+      </div>
+    </div>
   </div>
 </template>
 
--- resources/js/frontdesk/pages/workLoanV2.vue
+++ resources/js/frontdesk/pages/workLoanV2.vue
@@ -898,6 +898,15 @@
         </div>
       </div>
     </div>
+    <div class="product-info">
+      <div class="row no-gutters" style="justify-content: center; height: 50px;">
+        <span style="margin: auto 0; font-size: 16px; color: #023D64; font-weight: 700;">上班族貸</span>
+        <span class="ml-3" style="margin: auto 0; font-size: 14px; color: #023D64; opacity: 0.56; font-weight: 400;">•申貸金額：NT$ 10,000~500,000元</span>
+        <span class="ml-3" style="margin: auto 0; font-size: 14px; color: #023D64; opacity: 0.56; font-weight: 400;">•利率：上班族貸5%~16%</span>
+        <span class="ml-3" style="margin: auto 0; font-size: 14px; color: #023D64; opacity: 0.56; font-weight: 400;">•期數：3~24期</span>
+        <span class="ml-3" style="margin: auto 0; font-size: 14px; color: #023D64; opacity: 0.56; font-weight: 400;">•平台服務費：平台服務費最低500元，上班族貸4%</span>
+      </div>
+    </div>
   </div>
 </template>
 
