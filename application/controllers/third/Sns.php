e34bf050b02102a01ac62a1bc878832b23458ddb
chadwick <chadwick@influxfin.com>
2023-09-13 17:55:33 +0800

    format: 調整排版

--- application/controllers/third/Sns.php
+++ application/controllers/third/Sns.php
@@ -357,13 +357,13 @@ class Sns extends REST_Controller {
             }
         }
         $failed_rs = $this->s3_lib->failed_mail($s3_url, S3_BUCKET_MAILBOX);
-        if (!$failed_rs){
-            log_message('error',json_encode(['function_name'=>'failed_mail','message'=>'failed']));
+        if (!$failed_rs) {
+            log_message('error', json_encode(['function_name' => 'failed_mail', 'message' => 'failed']));
             throw new Exception('failed_mail failed');
         }
         $delete_rs = $this->s3_lib->public_delete_s3object($s3_url, S3_BUCKET_MAILBOX);
-        if (!$delete_rs){
-            log_message('error',json_encode(['function_name'=>'failed_mail','message'=>'failed']));
+        if (!$delete_rs) {
+            log_message('error', json_encode(['function_name' => 'failed_mail', 'message' => 'failed']));
             throw new Exception('failed_mail failed');
         }
         return true;
