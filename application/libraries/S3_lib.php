3ac82cb7ef15f5af9ca95dfbe41a2e55388865a6
chadwick <chadwick@influxfin.com>
2023-09-13 12:05:11 +0800

    feat: 把失敗的mail移動到failed資料夾

--- application/libraries/S3_lib.php
+++ application/libraries/S3_lib.php
@@ -174,6 +174,29 @@ class S3_lib {
             return false;
 		}
 	}
+    public function failed_mail($s3_url,$bucket=S3_BUCKET_MAILBOX)
+    {
+        $key=str_replace('https://'.$bucket.'.s3.us-west-2.amazonaws.com/','',$s3_url);
+        $filename = $this->public_get_filename($s3_url,$bucket);
+        $content  = file_get_contents('s3://'.$bucket.'/'.$filename);
+        if($content) {
+            try {
+                $result = $this->client_us2->putObject(array(
+                    'Bucket' => $bucket,
+                    'Key' => 'failed/' . $key,
+                    'Body' => $content
+                ));
+                return true;
+            } catch (Exception $e) {
+                error_log("failed_mail: {$e->getMessage()}, ($s3_url)");
+                return false;
+            }
+        }else{
+            error_log("failed_mail: The resource can't be accessed. ($s3_url)");
+            echo "failed_mail: The resource can't be accessed. ($s3_url)";
+            return false;
+        }
+    }
 	public function credit_mail_pdf($attachments, $user_id = 0, $name = 'credit', $type = 'test') : array
 	{
 		$images = [];
