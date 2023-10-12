dfa9ea31256870a30b1b7b576eac71b221d912b4
chadwick <chadwick@influxfin.com>
2023-10-12 21:33:19 +0800

    feat: 撈取相同負責人的公司列表 改成使用token，需要驗證token

--- application/controllers/api/v2/User.php
+++ application/controllers/api/v2/User.php
@@ -10,7 +10,7 @@ class User extends REST_Controller {
     {
         parent::__construct();
         $method 		= $this->router->fetch_method();
-        $nonAuthMethods = ['register','registerphone','login','login_new_app', 'sociallogin','smslogin','smsloginphone','forgotpw','credittest','biologin','fraud', 'user_behavior', 'charity_institutions','donate_anonymous', 'check_phone', 'company_list'];
+        $nonAuthMethods = ['register','registerphone','login','login_new_app', 'sociallogin','smslogin','smsloginphone','forgotpw','credittest','biologin','fraud', 'user_behavior', 'charity_institutions','donate_anonymous', 'check_phone'];
         if (!in_array($method, $nonAuthMethods)) {
             $token 		= isset($this->input->request_headers()['request_token'])?$this->input->request_headers()['request_token']:'';
             $tokenData 	= AUTHORIZATION::getUserInfoByToken($token);
