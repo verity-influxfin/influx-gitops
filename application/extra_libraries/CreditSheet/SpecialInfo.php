6d7704dc442c6a5ea63ffed627ce3b9a33344531
Chadwick Fu <chadwick@influxfin.com>
2024-01-05 11:02:30 +0800

    refactor: create SpecialInfo class with getInfo

---
+++ application/extra_libraries/CreditSheet/SpecialInfo.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CreditSheet;
+
+class SpecialInfo
+{
+    private $meta_keys = [
+        'job_company_taiwan_1000_point',
+        'job_company_world_500_point',
+        'job_company_medical_institute_point',
+        'job_company_public_agency_point',
+    ];
+    /**
+     * @var array
+     */
+    private $info = [];
+
+    /**
+     * @var int
+     */
+    private $target_id;
+
+    public function __construct(int $target_id)
+    {
+        $this->target_id = $target_id;
+        $target_meta = $this->getTargetMeta($this->target_id, $this->meta_keys);
+        $this->setInfo($target_meta);
+    }
+
+    /**
+     * @param int $target_id
+     * @param array $meta_keys
+     * @return array|false
+     */
+    public function getTargetMeta(int $target_id, array $meta_keys)
+    {
+        $target_meta = $this->CI->target_meta_model->as_array()->get_many_by([
+            'target_id' => $target_id,
+            'meta_key' => $meta_keys
+        ]);
+        return array_column($target_meta, 'meta_value', 'meta_key');
+    }
+
+    /**
+     * @return array
+     */
+    public function getInfo(): array
+    {
+        return $this->info;
+    }
+
+    /**
+     * @param $target_meta
+     * @return void
+     */
+    public function setInfo($target_meta): void
+    {
+        foreach ($this->meta_keys as $meta_key) {
+            $this->info[$meta_key] = $target_meta[$meta_key] ?? 0;
+        }
+    }
+}
