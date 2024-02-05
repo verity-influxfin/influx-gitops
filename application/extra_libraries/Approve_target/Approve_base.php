c4a3e76544eac2f6fa1131fbb8751f6485af9bb5
chadwick <chadwick@influxfin.com>
2024-02-05 21:23:08 +0800

    refactor: coding style

--- application/extra_libraries/Approve_target/Approve_base.php
+++ application/extra_libraries/Approve_target/Approve_base.php
@@ -222,7 +222,7 @@ abstract class Approve_base implements Approve_interface
             // 命中反詐欺
             return TRUE;
         }
-        if (isset($this->product_config['secondInstance']) && $this->product_config['secondInstance'] === TRUE)
+        if (($this->product_config['secondInstance'] ?? FALSE) === TRUE)
         {
             // 產品設定檔設定需二審
             return TRUE;
