8c808a5251b8fa2c6d2d2ba343dbe12cecb49859
chadwick <chadwick@influxfin.com>
2024-03-13 11:38:57 +0800

    fix: The result action set as "approve" as default. But it must be set as "cancel" for blocked user.

--- application/extra_libraries/Approve_target/Approve_base.php
+++ application/extra_libraries/Approve_target/Approve_base.php
@@ -114,6 +114,7 @@ abstract class Approve_base implements Approve_interface
             switch ($this->check_brookesia())
             {
                 case self::BROOKESIA_BLOCK:
+                    $this->result->set_action_cancel();
                     goto END;
                 case self::BROOKESIA_CLEAR:
                     break;
